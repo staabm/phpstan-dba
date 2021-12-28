101e8dfeb3baa9e27b779aa8dfb96d8d107ad33d
Markus Staab <m.staab@complex-it.de>
2021-12-28 17:55:10 +0100

    extracted QueryReflection

--- src/Extensions/PdoQueryDynamicReturnTypeExtension.php
+++ src/Extensions/PdoQueryDynamicReturnTypeExtension.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace staabm\PHPStanDba;
+namespace staabm\PHPStanDba\Extensions;
 
 use PDO;
 use PDOStatement;
@@ -10,17 +10,18 @@ use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\ConstantScalarType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
 use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\MixedType;
-use PHPStan\Type\Type;
-use PHPStan\Type\VerbosityLevel;
-use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\VerbosityLevel;
+use staabm\PHPStanDba\QueryReflection\QueryReflection;
 
 final class PdoQueryDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
@@ -52,7 +53,8 @@ final class PdoQueryDynamicReturnTypeExtension implements DynamicMethodReturnTyp
 		if ($queryType instanceof ConstantScalarType) {
 			$queryString = $queryType->getValue();
 
-			$resultType = $this->inferResultTypeFromQuery($queryString);
+			$queryReflection = new QueryReflection();
+			$resultType = $queryReflection->getResultType($queryString);
 			if ($resultType) {
 				//var_dump($resultType->describe(VerbosityLevel::precise()));
 				return $resultType;
@@ -60,22 +62,5 @@ final class PdoQueryDynamicReturnTypeExtension implements DynamicMethodReturnTyp
 		}
 
 		return $defaultReturn;
-	}
-
-	private function inferResultTypeFromQuery(string $queryString): ?Type
-	{
-		if ($queryString === 'SELECT name, id FROM foo') {
-			// XXX build dynamically
-			$arrayBuilder = ConstantArrayTypeBuilder::createEmpty();
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('id'), IntegerRangeType::fromInterval(0, null));
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('name'), new StringType());
-
-			//return TypeCombinator::union(
-			return	new GenericObjectType(PDOStatement::class, [$arrayBuilder->getArray()]);
-			//	new ConstantBooleanType(false)
-			//);
-		}
-
-		return null;
 	}
 }
---
+++ src/QueryReflection/QueryReflection.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace staabm\PHPStanDba\QueryReflection;
+
+use PDO;
+use PDOStatement;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\ConstantScalarType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\IntegerRangeType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\VerbosityLevel;
+
+final class QueryReflection {
+	public function __construct() {
+	}
+
+	public function getResultType(string $queryString):?Type {
+		if ($queryString === 'SELECT name, id FROM foo') {
+			// XXX build dynamically
+			$arrayBuilder = ConstantArrayTypeBuilder::createEmpty();
+			$arrayBuilder->setOffsetValueType(new ConstantStringType('id'), IntegerRangeType::fromInterval(0, null));
+			$arrayBuilder->setOffsetValueType(new ConstantStringType('name'), new StringType());
+
+			return TypeCombinator::union(
+				new GenericObjectType(PDOStatement::class, [$arrayBuilder->getArray()]),
+				new ConstantBooleanType(false)
+			);
+		}
+
+		return null;
+	}
+}
