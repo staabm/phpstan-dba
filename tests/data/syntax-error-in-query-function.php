eedc48cbcd0c47b146346de488a1a04eb7b24fc5
Markus Staab <m.staab@complex-it.de>
2021-12-30 16:42:28 +0100

    added valid query case

--- tests/data/syntax-error-in-query-function.php
+++ tests/data/syntax-error-in-query-function.php
@@ -10,4 +10,9 @@ class Foo
     {
         \Deployer\runMysqlQuery('SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada', $dbCredentials);
     }
+
+    public function validQuery(DbCredentials $dbCredentials)
+    {
+        \Deployer\runMysqlQuery('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada', $dbCredentials);
+    }
 }
