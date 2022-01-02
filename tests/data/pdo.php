4ee47dccb40b3b0b1fbdc5315d437985bb89d09d
Markus Staab <maggus.staab@googlemail.com>
2022-01-02 11:05:28 +0100

    Update pdo.php

--- tests/data/pdo.php
+++ tests/data/pdo.php
@@ -9,7 +9,7 @@ use function PHPStan\Testing\assertType;
 class Foo
 {
 	public function dynamicTableQuery(PDO $pdo, int $typeId) {
-		$stmt = $pdo->query(SELECT * FROM ' . rex::getTablePrefix() . 'media_manager_type WHERE id=' . $typeId, PDO::FETCH_ASSOC);
+		$stmt = $pdo->query('SELECT * FROM ' . rex::getTablePrefix() . 'media_manager_type WHERE id=' . $typeId, PDO::FETCH_ASSOC);
 		assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>}>', $stmt);
 	}
 }
