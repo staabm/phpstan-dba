4ed45dac997927f6a080a3e1df3c79c7899b4760
Markus Staab <m.staab@complex-it.de>
2022-01-03 11:13:09 +0100

    report unknown-table error when no reflector knows about the query-string

--- src/QueryReflection/ChainedReflector.php
+++ src/QueryReflection/ChainedReflector.php
@@ -24,13 +24,26 @@ final class ChainedReflector implements QueryReflector
 
     public function validateQueryString(string $simulatedQueryString): ?Error
     {
+        $nooneKnows = true;
+
         foreach ($this->reflectors as $reflector) {
             $reflectorError = $reflector->validateQueryString($simulatedQueryString);
 
             // on "not found" error, we try the next reflector.
-            if ($reflectorError && !\in_array($reflectorError->getCode(), [MysqliQueryReflector::MYSQL_UNKNOWN_TABLE])) {
-                return $reflectorError;
+            if ($reflectorError) {
+                if (!\in_array($reflectorError->getCode(), [MysqliQueryReflector::MYSQL_UNKNOWN_TABLE])) {
+                    return $reflectorError;
+                }
+                if (true === $nooneKnows) {
+                    $nooneKnows = $reflectorError;
+                }
+            } else {
+                $nooneKnows = false;
             }
+        }
+
+        if ($nooneKnows instanceof Error) {
+            return $nooneKnows;
         }
 
         return null;
