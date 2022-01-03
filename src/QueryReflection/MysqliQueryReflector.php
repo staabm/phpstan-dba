844626b4b31d8e8b180c8f371985cbd458f7633d
Markus Staab <m.staab@complex-it.de>
2022-01-03 17:43:58 +0100

    fix

--- src/QueryReflection/MysqliQueryReflector.php
+++ src/QueryReflection/MysqliQueryReflector.php
@@ -6,6 +6,7 @@ namespace staabm\PHPStanDba\QueryReflection;
 
 use mysqli;
 use mysqli_result;
+use mysqli_report;
 use mysqli_sql_exception;
 use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
 use PHPStan\Type\Constant\ConstantIntegerType;
@@ -46,7 +47,7 @@ final class MysqliQueryReflector implements QueryReflector
         // set a sane default.. atm this should not have any impact
         $this->db->set_charset('utf8');
 		// enable exception throwing on php <8.1
-		$this->db->report_mode = MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT;
+		mysqli_report(MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT);
 
         $this->nativeTypes = [];
         $this->nativeFlags = [];
