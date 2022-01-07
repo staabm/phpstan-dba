0d0c0996a4a6ccc046c6b9bd5841ef526e889592
Markus Staab <m.staab@complex-it.de>
2022-01-07 09:05:16 +0100

    infer PDOStatement generic in $PDO->prepare() with plain queries

---
+++ tests/data/pdo-prepare.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PdoPrepareTest;
+
+use PDO;
+use function PHPStan\Testing\assertType;
+
+class Foo
+{
+    public function prepareSelected(PDO $pdo)
+    {
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada');
+        $stmt->execute();
+        assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+
+        foreach ($stmt as $row) {
+            assertType('int<0, 4294967295>', $row['adaid']);
+            assertType('string', $row['email']);
+        }
+    }
+}
--- tests/data/pdo.php
+++ tests/data/pdo.php
@@ -73,7 +73,7 @@ class Foo
         $stmt = $pdo->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada WHERE adaid='.$bool, PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>', $stmt);
 
-        // ---- too queries, for which we cannot infer the return type
+        // ---- queries, for which we cannot infer the return type
 
         $stmt = $pdo->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada WHERE '.$string, PDO::FETCH_ASSOC);
         assertType('PDOStatement<array>|false', $stmt);
