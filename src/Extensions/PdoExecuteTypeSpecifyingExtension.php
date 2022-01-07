e6748b55fde476f6f9a296db653287fb4c909ff5
Markus Staab <m.staab@complex-it.de>
2022-01-07 17:58:50 +0100

    initial implementation

---
+++ src/Extensions/PdoExecuteTypeSpecifyingExtension.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\Extensions;
+
+use PDOStatement;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Analyser\TypeSpecifier;
+use PHPStan\Analyser\TypeSpecifierAwareExtension;
+use PHPStan\Analyser\TypeSpecifierContext;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\ConstantScalarType;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\MethodTypeSpecifyingExtension;
+use staabm\PHPStanDba\QueryReflection\QueryReflection;
+use staabm\PHPStanDba\QueryReflection\QueryReflector;
+
+final class PdoExecuteTypeSpecifyingExtension implements MethodTypeSpecifyingExtension, TypeSpecifierAwareExtension
+{
+	private TypeSpecifier $typeSpecifier;
+
+	private $nodeFinder;
+
+	/*
+	public function __construct(BetterNodeFinder $nodeFinder) {
+		$this->nodeFinder = $nodeFinder;
+	}
+	*/
+
+	public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
+	{
+		$this->typeSpecifier = $typeSpecifier;
+	}
+
+    public function getClass(): string
+    {
+        return PDOStatement::class;
+    }
+
+	public function isMethodSupported(MethodReflection $methodReflection, MethodCall $node, TypeSpecifierContext $context): bool
+	{
+		return 'execute' === $methodReflection->getName();
+	}
+
+	public function specifyTypes(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope, TypeSpecifierContext $context): \PHPStan\Analyser\SpecifiedTypes
+	{
+		$stmtType = $scope->getType($methodCall->var);
+		$args = $methodCall->getArgs();
+
+		if (count($args) === 0) {
+			return $this->typeSpecifier->create($methodCall->var, $stmtType, TypeSpecifierContext::createTruthy());
+		}
+
+		$placeholders = array();
+		$parameterTypes = $scope->getType($args[0]->value);
+		if ($parameterTypes instanceof ConstantArrayType) {
+			$keyTypes = $parameterTypes->getKeyTypes();
+			$valueTypes = $parameterTypes->getValueTypes();
+
+			$placeholders = array();
+			foreach($keyTypes as $i => $keyType) {
+				if ($keyType instanceof ConstantStringType) {
+					$placeholderName = $keyType->getValue();
+
+					if (!str_starts_with($placeholderName, ':')) {
+						$placeholderName = ':' . $placeholderName;
+					}
+
+					if ($valueTypes[$i] instanceof ConstantScalarType) {
+						$placeholders[$placeholderName] = (string) ($valueTypes[$i]->getValue());
+					}
+				}
+			}
+		}
+
+		$queryString = 'SELECT email, adaid FROM ada WHERE adaid = :adaid';
+		foreach($placeholders as $placeholderName => $value) {
+			$queryString = str_replace($placeholderName, $value, $queryString);
+		}
+
+		$queryReflection = new QueryReflection();
+		// $queryString = $queryReflection->resolveQueryString($args[0]->value, $scope);
+
+		$reflectionFetchType = QueryReflector::FETCH_TYPE_BOTH;
+		$resultType = $queryReflection->getResultType($queryString, $reflectionFetchType);
+
+		if ($resultType) {
+			$stmtType = new GenericObjectType(PDOStatement::class, [$resultType]);
+		}
+
+		return $this->typeSpecifier->create($methodCall->var, $stmtType, TypeSpecifierContext::createTruthy(), true);
+	}
+
+}
--- tests/data/pdo-stmt-execute.php
+++ tests/data/pdo-stmt-execute.php
@@ -12,5 +12,11 @@ class Foo
         $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
         $stmt->execute(array(':adaid' => 1));
 		assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+
+		/*
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
+        $stmt->execute(array('adaid' => 1)); // prefixed ":" is optional
+		assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+		*/
 	}
 }
