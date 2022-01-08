6b67f1c02ea7e8e548021410721735188a043ada
Markus Staab <m.staab@complex-it.de>
2022-01-08 21:13:26 +0100

    fix test

--- tests/data/pdo-stmt-execute.php
+++ tests/data/pdo-stmt-execute.php
@@ -10,8 +10,11 @@ class Foo
     public function execute(PDO $pdo)
     {
         $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
-        $stmt->execute([':adaid' => 1]);
-        assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+		assertType('PDOStatement|false', $stmt);
+		if ($stmt) {
+			$stmt->execute([':adaid' => 1]);
+			assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+		}
 
         /*
         $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
