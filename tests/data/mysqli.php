33d068b66c2a3c241786e9851320d2e2c924acfb
Markus Staab <m.staab@complex-it.de>
2022-01-08 22:23:20 +0100

    fix

--- tests/data/mysqli.php
+++ tests/data/mysqli.php
@@ -10,7 +10,7 @@ class Foo
     public function ooQuerySelected(mysqli $mysqli)
     {
         $result = $mysqli->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
-        assertType('mysqli_result<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>|false', $result);
+        assertType('mysqli_result<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>', $result);
 
         if ($result) {
             foreach ($result as $row) {
@@ -31,7 +31,7 @@ class Foo
     public function fnQuerySelected(mysqli $mysqli)
     {
         $result = mysqli_query($mysqli, 'SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
-        assertType('mysqli_result<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>|false', $result);
+        assertType('mysqli_result<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>', $result);
 
         if ($result) {
             foreach ($result as $row) {
