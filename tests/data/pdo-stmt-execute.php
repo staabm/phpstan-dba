ee92a8eacf13fe842ce63a5cab048594e29e9254
Markus Staab <m.staab@complex-it.de>
2022-01-09 13:29:21 +0100

    test coverage for named placeholder

--- tests/data/pdo-stmt-execute.php
+++ tests/data/pdo-stmt-execute.php
@@ -19,6 +19,11 @@ class Foo
         $stmt->execute(['adaid' => 1]); // prefixed ":" is optional
         assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
 
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE email = :email');
+        assertType('PDOStatement', $stmt);
+        $stmt->execute([':email' => 'email@example.org']);
+        assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}>', $stmt);
+
         $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ?');
         assertType('PDOStatement', $stmt);
         $stmt->execute([1]);
