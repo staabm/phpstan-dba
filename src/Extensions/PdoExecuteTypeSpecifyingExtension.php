44e108167301a8b12aad24317f49e4b7a5a275ca
Markus Staab <m.staab@complex-it.de>
2022-01-10 11:39:24 +0100

    extracted `PdoStatementReflection`

--- src/Extensions/PdoExecuteTypeSpecifyingExtension.php
+++ src/Extensions/PdoExecuteTypeSpecifyingExtension.php
@@ -5,35 +5,23 @@ declare(strict_types=1);
 namespace staabm\PHPStanDba\Extensions;
 
 use PDOStatement;
-use PhpParser\Node;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\FunctionLike;
-use PhpParser\NodeFinder;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
 use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierAwareExtension;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\Reflection\MethodReflection;
-use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\MethodTypeSpecifyingExtension;
 use PHPStan\Type\Type;
+use staabm\PHPStanDba\PdoReflection\PdoStatementReflection;
 use staabm\PHPStanDba\QueryReflection\QueryReflection;
 use staabm\PHPStanDba\QueryReflection\QueryReflector;
-use Symplify\Astral\ValueObject\AttributeKey;
 
 final class PdoExecuteTypeSpecifyingExtension implements MethodTypeSpecifyingExtension, TypeSpecifierAwareExtension
 {
     private TypeSpecifier $typeSpecifier;
-    private NodeFinder $nodeFinder;
-
-    public function __construct()
-    {
-        $this->nodeFinder = new NodeFinder();
-    }
 
     public function getClass(): string
     {
@@ -72,7 +60,8 @@ final class PdoExecuteTypeSpecifyingExtension implements MethodTypeSpecifyingExt
             return null;
         }
 
-        $queryExpr = $this->findQueryStringExpression($methodCall);
+        $stmtReflection = new PdoStatementReflection();
+        $queryExpr = $stmtReflection->findQueryStringExpression($methodCall);
         if (null === $queryExpr) {
             return null;
         }
@@ -99,75 +88,5 @@ final class PdoExecuteTypeSpecifyingExtension implements MethodTypeSpecifyingExt
         }
 
         return null;
-    }
-
-    private function findQueryStringExpression(MethodCall $methodCall): ?Expr
-    {
-        // todo: use astral simpleNameResolver
-        $nameResolver = function ($node) {
-            if (\is_string($node->name)) {
-                return $node->name;
-            }
-            if ($node->name instanceof Node\Identifier) {
-                return $node->name->toString();
-            }
-        };
-
-        $current = $methodCall;
-        while (null !== $current) {
-            /** @var Assign|null $assign */
-            $assign = $this->findFirstPreviousOfNode($current, function ($node) {
-                return $node instanceof Assign;
-            });
-
-            if (null !== $assign && $nameResolver($assign->var) === $nameResolver($methodCall->var)) {
-                return $assign->expr;
-            }
-
-            $current = $assign;
-        }
-
-        return null;
-    }
-
-    /**
-     * @param callable(Node $node):bool $filter
-     */
-    private function findFirstPreviousOfNode(Node $node, callable $filter): ?Node
-    {
-        // move to previous expression
-        $previousStatement = $node->getAttribute(AttributeKey::PREVIOUS);
-        if (null !== $previousStatement) {
-            if (!$previousStatement instanceof Node) {
-                throw new ShouldNotHappenException();
-            }
-            $foundNode = $this->findFirst([$previousStatement], $filter);
-            // we found what we need
-            if (null !== $foundNode) {
-                return $foundNode;
-            }
-
-            return $this->findFirstPreviousOfNode($previousStatement, $filter);
-        }
-
-        $parent = $node->getAttribute(AttributeKey::PARENT);
-        if ($parent instanceof FunctionLike) {
-            return null;
-        }
-
-        if ($parent instanceof Node) {
-            return $this->findFirstPreviousOfNode($parent, $filter);
-        }
-
-        return null;
-    }
-
-    /**
-     * @param Node|Node[] $nodes
-     * @param callable(Node $node):bool $filter
-     */
-    private function findFirst(Node|array $nodes, callable $filter): ?Node
-    {
-        return $this->nodeFinder->findFirst($nodes, $filter);
     }
 }
