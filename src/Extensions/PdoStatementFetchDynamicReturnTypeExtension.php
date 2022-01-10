6231e31b623ae7c76c2c6e43abe8ec3a805d1da7
Markus Staab <maggus.staab@googlemail.com>
2022-01-10 11:02:34 +0100

    refactor `PdoStatementFetchDynamicReturnTypeExtension` (#57)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- src/Extensions/PdoStatementFetchDynamicReturnTypeExtension.php
+++ src/Extensions/PdoStatementFetchDynamicReturnTypeExtension.php
@@ -50,73 +50,75 @@ final class PdoStatementFetchDynamicReturnTypeExtension implements DynamicMethod
 
     public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
     {
-        $args = $methodCall->getArgs();
         $defaultReturn = ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
 
         if (QueryReflection::getRuntimeConfiguration()->throwsPdoExceptions($this->phpVersion) && !$defaultReturn instanceof MixedType) {
             $defaultReturn = TypeCombinator::remove($defaultReturn, new ConstantBooleanType(false));
         }
 
-        $statementType = $scope->getType($methodCall->var);
-
-        if ($statementType instanceof GenericObjectType) {
-            $genericTypes = $statementType->getTypes();
-
-            if (1 !== \count($genericTypes)) {
-                return $defaultReturn;
-            }
-
-            $fetchType = PDO::FETCH_BOTH;
-            if (\count($args) > 0) {
-                $fetchModeType = $scope->getType($args[0]->value);
-                if (!$fetchModeType instanceof ConstantIntegerType) {
-                    return $defaultReturn;
-                }
-                $fetchType = $fetchModeType->getValue();
-
-                if (!\in_array($fetchType, [PDO::FETCH_ASSOC, PDO::FETCH_NUM, PDO::FETCH_BOTH])) {
-                    return $defaultReturn;
-                }
-            }
-
-            $resultType = $genericTypes[0];
-
-            if ((PDO::FETCH_NUM === $fetchType || PDO::FETCH_ASSOC === $fetchType) && $resultType instanceof ConstantArrayType) {
-                $builder = ConstantArrayTypeBuilder::createEmpty();
-
-                $keyTypes = $resultType->getKeyTypes();
-                $valueTypes = $resultType->getValueTypes();
-
-                foreach ($keyTypes as $i => $keyType) {
-                    if (PDO::FETCH_NUM === $fetchType && $keyType instanceof ConstantIntegerType) {
-                        $builder->setOffsetValueType($keyType, $valueTypes[$i]);
-                    } elseif (PDO::FETCH_ASSOC === $fetchType && $keyType instanceof ConstantStringType) {
-                        $builder->setOffsetValueType($keyType, $valueTypes[$i]);
-                    }
-                }
-
-                if ('fetchAll' === $methodReflection->getName()) {
-                    return new ArrayType(new IntegerType(), $builder->getArray());
-                }
-
-                if (QueryReflection::getRuntimeConfiguration()->throwsPdoExceptions($this->phpVersion)) {
-                    return $builder->getArray();
-                }
-
-                return new UnionType([$builder->getArray(), new ConstantBooleanType(false)]);
-            }
-
-            if ('fetchAll' === $methodReflection->getName()) {
-                return new ArrayType(new IntegerType(), $resultType);
-            }
-
-            if (QueryReflection::getRuntimeConfiguration()->throwsPdoExceptions($this->phpVersion)) {
-                return $resultType;
-            }
-
-            return new UnionType([$resultType, new ConstantBooleanType(false)]);
+        $resultType = $this->inferType($methodReflection, $methodCall, $scope);
+        if (null !== $resultType) {
+            return $resultType;
         }
 
         return $defaultReturn;
+    }
+
+    private function inferType(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
+    {
+        $args = $methodCall->getArgs();
+        $statementType = $scope->getType($methodCall->var);
+
+        if (!$statementType instanceof GenericObjectType) {
+            return null;
+        }
+
+        $genericTypes = $statementType->getTypes();
+
+        if (1 !== \count($genericTypes)) {
+            return null;
+        }
+
+        $fetchType = PDO::FETCH_BOTH;
+        if (\count($args) > 0) {
+            $fetchModeType = $scope->getType($args[0]->value);
+            if (!$fetchModeType instanceof ConstantIntegerType) {
+                return null;
+            }
+            $fetchType = $fetchModeType->getValue();
+
+            if (!\in_array($fetchType, [PDO::FETCH_ASSOC, PDO::FETCH_NUM, PDO::FETCH_BOTH])) {
+                return null;
+            }
+        }
+
+        $resultType = $genericTypes[0];
+
+        if ((PDO::FETCH_NUM === $fetchType || PDO::FETCH_ASSOC === $fetchType) && $resultType instanceof ConstantArrayType) {
+            $builder = ConstantArrayTypeBuilder::createEmpty();
+
+            $keyTypes = $resultType->getKeyTypes();
+            $valueTypes = $resultType->getValueTypes();
+
+            foreach ($keyTypes as $i => $keyType) {
+                if (PDO::FETCH_NUM === $fetchType && $keyType instanceof ConstantIntegerType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                } elseif (PDO::FETCH_ASSOC === $fetchType && $keyType instanceof ConstantStringType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                }
+            }
+
+            $resultType = $builder->getArray();
+        }
+
+        if ('fetchAll' === $methodReflection->getName()) {
+            return new ArrayType(new IntegerType(), $resultType);
+        }
+
+        if (QueryReflection::getRuntimeConfiguration()->throwsPdoExceptions($this->phpVersion)) {
+            return $resultType;
+        }
+
+        return new UnionType([$resultType, new ConstantBooleanType(false)]);
     }
 }
