a6bbf027480e0085423de3b3b06916e585ce49c6
Markus Staab <m.staab@complex-it.de>
2022-01-10 16:00:46 +0100

    simplify

--- demo.php
+++ demo.php
@@ -2,18 +2,24 @@
 
 function demo(\PDO $pdo): void
 {
+	$pdo->query('SELECT * FROM unknownTable', PDO::FETCH_ASSOC);
+
+	$pdo->query('SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada', PDO::FETCH_ASSOC);
+
+	$pdo->query('SELECT doesNotExist, adaid, gesperrt, freigabe1u1 FROM ada', PDO::FETCH_ASSOC);
+
 	$stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
 	$stmt->execute([':wrongParamName' => 1]);
 
 	$stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
-	$stmt->execute(); // missing parameter
+	$stmt->execute();
 
 	$stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ? and email = ?');
-	$stmt->execute([1]); // wrong number of parameters
+	$stmt->execute([1]);
 
 	$stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid and email = :email');
-	$stmt->execute(['adaid' => 1]); // wrong number of parameters
+	$stmt->execute(['adaid' => 1]);
 
 	$stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid and email = :email');
-	$stmt->execute([':email' => 'email@example.org']); // wrong number of parameters
+	$stmt->execute([':email' => 'email@example.org']);
 }
