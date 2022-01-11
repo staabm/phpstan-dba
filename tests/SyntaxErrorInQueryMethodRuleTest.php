fc66b666f57137d607e058e6020c6d47be1d6fc1
Markus Staab <m.staab@complex-it.de>
2022-01-11 16:50:52 +0100

    fix

--- tests/SyntaxErrorInQueryMethodRuleTest.php
+++ tests/SyntaxErrorInQueryMethodRuleTest.php
@@ -51,7 +51,7 @@ class SyntaxErrorInQueryMethodRuleTest extends AbstractServiceAwareRuleTestCase
             ],
             [
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
-                75,
+                67,
             ],
         ]);
     }
