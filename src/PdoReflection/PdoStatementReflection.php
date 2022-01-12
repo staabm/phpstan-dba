f0edf7807cc7c3458c1d3de693587f42789c84d1
Markus Staab <m.staab@complex-it.de>
2022-01-12 11:10:48 +0100

    kill cs

--- src/PdoReflection/PdoStatementReflection.php
+++ src/PdoReflection/PdoStatementReflection.php
@@ -32,8 +32,7 @@ final class PdoStatementReflection
 
     private NodeFinder $nodeFinder;
 
-    public function __construct()
-    {
+    public function __construct()    {
         $this->nodeFinder = new NodeFinder();
     }
 
