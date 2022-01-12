caa9824e596f0941b2d49b1e67d12082a6696bc7
Markus Staab <maggus.staab@googlemail.com>
2022-01-12 11:55:03 +0100

    fixed detection of syntax errors in prepared statements on konwn parameter types (#91)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- src/QueryReflection/QueryReflection.php
+++ src/QueryReflection/QueryReflection.php
@@ -7,18 +7,10 @@ namespace staabm\PHPStanDba\QueryReflection;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\BinaryOp\Concat;
 use PHPStan\Analyser\Scope;
-use PHPStan\Type\BooleanType;
 use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\Constant\ConstantStringType;
-use PHPStan\Type\ConstantScalarType;
-use PHPStan\Type\FloatType;
-use PHPStan\Type\IntegerType;
-use PHPStan\Type\IntersectionType;
-use PHPStan\Type\MixedType;
-use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
-use PHPStan\Type\UnionType;
 use staabm\PHPStanDba\DbaException;
 use staabm\PHPStanDba\Error;
 
@@ -106,34 +98,8 @@ final class QueryReflection
         }
 
         $type = $scope->getType($queryExpr);
-        if ($type instanceof ConstantScalarType) {
-            return (string) $type->getValue();
-        }
 
-        $integerType = new IntegerType();
-        if ($integerType->isSuperTypeOf($type)->yes()) {
-            return '1';
-        }
-
-        $booleanType = new BooleanType();
-        if ($booleanType->isSuperTypeOf($type)->yes()) {
-            return '1';
-        }
-
-        if ($type->isNumericString()->yes()) {
-            return '1';
-        }
-
-        $floatType = new FloatType();
-        if ($floatType->isSuperTypeOf($type)->yes()) {
-            return '1.0';
-        }
-
-        if ($type instanceof MixedType || $type instanceof StringType || $type instanceof IntersectionType || $type instanceof UnionType) {
-            return null;
-        }
-
-        throw new DbaException(sprintf('Unexpected expression type %s', \get_class($type)));
+        return QuerySimulation::simulateParamValueType($type);
     }
 
     private function getQueryType(string $query): ?string
@@ -166,17 +132,9 @@ final class QueryReflection
                         $placeholderName = ':'.$placeholderName;
                     }
 
-                    if ($valueTypes[$i] instanceof ConstantScalarType) {
-                        $parameters[$placeholderName] = $valueTypes[$i]->getValue();
-                    } else {
-                        return null;
-                    }
+                    $parameters[$placeholderName] = QuerySimulation::simulateParamValueType($valueTypes[$i]);
                 } elseif ($keyType instanceof ConstantIntegerType) {
-                    if ($valueTypes[$i] instanceof ConstantScalarType) {
-                        $parameters[$keyType->getValue()] = $valueTypes[$i]->getValue();
-                    } else {
-                        return null;
-                    }
+                    $parameters[$keyType->getValue()] = QuerySimulation::simulateParamValueType($valueTypes[$i]);
                 }
             }
 
