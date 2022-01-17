7c90f70ed692a0add6b828d353d86b3e39f19fc9
Markus Staab <m.staab@complex-it.de>
2022-01-17 14:04:52 +0100

    Added Doctrine Dbal type inference

---
+++ tests/data/doctrine-dbal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DoctrineDbalTest;
+
+use Doctrine\DBAL\Driver\Connection;
+use function PHPStan\Testing\assertType;
+
+class Foo {
+    public function foo(Connection $conn) {
+        $result = $conn->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
+        assertType('Doctrine\DBAL\ResultDoctrine\DBAL\Result<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>, gesperrt: int<-128, 127>, 2: int<-128, 127>, freigabe1u1: int<-128, 127>, 3: int<-128, 127>}>', $result);
+    }
+}
