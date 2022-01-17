1a2eca603cb28c324d39bfb2ffafc9a94299f07f
Markus Staab <m.staab@complex-it.de>
2022-01-17 14:05:25 +0100

    fix

--- tests/data/doctrine-dbal.php
+++ tests/data/doctrine-dbal.php
@@ -8,6 +8,6 @@ use function PHPStan\Testing\assertType;
 class Foo {
     public function foo(Connection $conn) {
         $result = $conn->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
-        assertType('Doctrine\DBAL\ResultDoctrine\DBAL\Result<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>, gesperrt: int<-128, 127>, 2: int<-128, 127>, freigabe1u1: int<-128, 127>, 3: int<-128, 127>}>', $result);
+        assertType('Doctrine\DBAL\Result<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>, gesperrt: int<-128, 127>, 2: int<-128, 127>, freigabe1u1: int<-128, 127>, 3: int<-128, 127>}>', $result);
     }
 }
