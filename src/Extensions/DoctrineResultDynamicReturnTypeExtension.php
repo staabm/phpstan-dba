bc99db11acc56177eaf0aa56df15c447a7ba85e4
Markus Staab <m.staab@complex-it.de>
2022-01-17 14:19:19 +0100

    support Result->fetchNumeric()

---
+++ src/Extensions/DoctrineResultDynamicReturnTypeExtension.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\Extensions;
+
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Result;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\Type;
+use staabm\PHPStanDba\QueryReflection\QueryReflection;
+use staabm\PHPStanDba\QueryReflection\QueryReflector;
+
+final class DoctrineResultDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return Result::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return in_array(strtolower($methodReflection->getName()), ['fetchnumeric'], true);
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        $defaultReturn = ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+
+        $resultType = $scope->getType($methodCall->var);
+
+        if (!$resultType instanceof GenericObjectType) {
+            return $defaultReturn;
+        }
+
+        $genericTypes = $resultType->getTypes();
+
+        if (1 !== \count($genericTypes)) {
+            return $defaultReturn;
+        }
+
+        switch(strtolower($methodReflection->getName())) {
+            case 'fetchnumeric':
+                $fetchType = QueryReflector::FETCH_TYPE_NUMERIC;
+                break;
+            default:
+                $fetchType = QueryReflector::FETCH_TYPE_BOTH;
+        }
+
+        $resultType = $genericTypes[0];
+
+        if ((QueryReflector::FETCH_TYPE_NUMERIC === $fetchType || QueryReflector::FETCH_TYPE_ASSOC === $fetchType) && $resultType instanceof ConstantArrayType) {
+            $builder = ConstantArrayTypeBuilder::createEmpty();
+
+            $keyTypes = $resultType->getKeyTypes();
+            $valueTypes = $resultType->getValueTypes();
+
+            foreach ($keyTypes as $i => $keyType) {
+                if (QueryReflector::FETCH_TYPE_NUMERIC === $fetchType && $keyType instanceof ConstantIntegerType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                } elseif (QueryReflector::FETCH_TYPE_ASSOC === $fetchType && $keyType instanceof ConstantStringType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                }
+            }
+
+            return new ArrayType(new IntegerType(), $builder->getArray());
+        }
+
+        return $resultType;
+    }
+}
--- tests/data/doctrine-dbal.php
+++ tests/data/doctrine-dbal.php
@@ -11,5 +11,8 @@ class Foo
     {
         $result = $conn->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
         assertType('Doctrine\DBAL\Result<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>, gesperrt: int<-128, 127>, 2: int<-128, 127>, freigabe1u1: int<-128, 127>, 3: int<-128, 127>}>', $result);
+
+        $numericFetch = $result->fetchNumeric();
+        assertType('array<int, array{string, int<0, 4294967295>, int<-128, 127>, int<-128, 127>}>', $numericFetch);
     }
 }
