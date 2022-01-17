63467c57f2ee43bf0e5ea9b5a90b0d10d82e2e17
Markus Staab <m.staab@complex-it.de>
2022-01-17 14:57:39 +0100

    fix

--- tests/SyntaxErrorInPreparedStatementMethodRuleTest.php
+++ tests/SyntaxErrorInPreparedStatementMethodRuleTest.php
@@ -39,15 +39,15 @@ class SyntaxErrorInPreparedStatementMethodRuleTest extends AbstractServiceAwareR
             ],
             [
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
-                104,
-            ],
-            [
-                "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
                 105,
             ],
             [
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
                 106,
+            ],
+            [
+                "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
+                107,
             ],
         ]);
     }
