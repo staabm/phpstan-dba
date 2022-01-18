607c392859775c0e39699e28b98a7a681d7513ff
Markus Staab <m.staab@complex-it.de>
2022-01-18 09:49:04 +0100

    fix tests

--- tests/SyntaxErrorInQueryMethodRuleTest.php
+++ tests/SyntaxErrorInQueryMethodRuleTest.php
@@ -50,16 +50,16 @@ class SyntaxErrorInQueryMethodRuleTest extends AbstractServiceAwareRuleTestCase
                 41,
             ],
             [
-                "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'WHERE email='my_other_table' LIMIT 0' at line 1 (1064).",
+                "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'FROM ada  WHERE email='my_other_table' LIMIT 0' at line 1 (1064).",
                 56,
             ],
             [
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
-                66,
+                76,
             ],
             [
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
-                72,
+                82,
             ],
         ]);
     }
