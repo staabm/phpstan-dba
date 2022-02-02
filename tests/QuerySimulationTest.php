4e094082e46a7c68a7278bfa3babbf5f27b25e89
Markus Staab <maggus.staab@googlemail.com>
2022-02-02 22:43:39 +0100

    more test coverage

--- tests/QuerySimulationTest.php
+++ tests/QuerySimulationTest.php
@@ -4,16 +4,28 @@ namespace staabm\PHPStanDba\Tests;
 
 use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
 use PHPStan\Type\IntegerType;
+use PHPStan\Type\StringType;
 use PHPStan\Type\VerbosityLevel;
 use PHPUnit\Framework\TestCase;
 use staabm\PHPStanDba\QueryReflection\QuerySimulation;
 
 class QuerySimulationTest extends TestCase
 {
-    public function testIntersectionType()
+    public function testIntersectionTypeInt()
     {
+        // non-empty-array<int, int>
         $builder = ConstantArrayTypeBuilder::createEmpty();
         $builder->setOffsetValueType(new IntegerType(), new IntegerType());
+
+        $simulatedValue = QuerySimulation::simulateParamValueType($builder->getArray(), false);
+        $this->assertNotNull($simulatedValue);
+    }
+
+    public function testIntersectionTypeString()
+    {
+        // non-empty-array<string, int>
+        $builder = ConstantArrayTypeBuilder::createEmpty();
+        $builder->setOffsetValueType(new StringType(), new IntegerType());
 
         $simulatedValue = QuerySimulation::simulateParamValueType($builder->getArray(), false);
         $this->assertNotNull($simulatedValue);
