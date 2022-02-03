e229dbb4ef69b96a0898578f5e44b42e21ca0d00
Markus Staab <m.staab@complex-it.de>
2022-02-03 10:00:22 +0100

    Update MysqliQueryReflector.php

--- src/QueryReflection/MysqliQueryReflector.php
+++ src/QueryReflection/MysqliQueryReflector.php
@@ -61,11 +61,10 @@ final class MysqliQueryReflector implements QueryReflector
         $this->nativeFlags = [];
 
         $constants = get_defined_constants(true);
-        var_dump($constants['mysqli']);
         foreach ($constants['mysqli'] as $c => $n) {
-            var_dump($n);
             if (!\is_int($n)) {
-                throw new ShouldNotHappenException();
+                // skip bool constants like MYSQLI_IS_MARIADB
+                continue;
             }
             if (preg_match('/^MYSQLI_TYPE_(.*)/', $c, $m)) {
                 if (!\is_string($m[1])) {
