dc7f06e46eb5482b62fc5fe7183f445a18a58a17
Markus Staab <maggus.staab@googlemail.com>
2022-02-07 17:10:27 +0100

    docs

--- tests/fetchMode/data/modes.php
+++ tests/fetchMode/data/modes.php
@@ -9,6 +9,8 @@ class Foo
 {
     public function assocMode(PDO $pdo)
     {
+        // default fetch-type is globally changed to assoc for this test-suite
+        
         $stmt = $pdo->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada');
         assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}>', $stmt);
     }
