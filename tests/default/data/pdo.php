cdfc586f9a60939ff19f58866e48ef66a8048114
Markus Staab <m.staab@complex-it.de>
2022-02-09 12:00:35 +0100

    more testcoverage

--- tests/default/data/pdo.php
+++ tests/default/data/pdo.php
@@ -3,6 +3,7 @@
 namespace PdoTest;
 
 use PDO;
+use function PHPStan\dumpType;
 use function PHPStan\Testing\assertType;
 
 class Foo
@@ -251,9 +252,13 @@ class Foo
 
         $query = 'SELECT email, adaid FROM ada';
         $stmt = $pdo->query($query);
+        assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}'. $bothType .'>', $stmt);
 
         $stmt->setFetchMode(PDO::FETCH_NUM);
         assertType('PDOStatement<array{string, int<0, 4294967295>}'. $bothType .'>', $stmt);
+
+        $result = $stmt->fetch(PDO::FETCH_NUM);
+        assertType('array{string, int<0, 4294967295>}', $result);
     }
 
     public function setFetchModeAssoc(PDO $pdo)
@@ -262,8 +267,27 @@ class Foo
 
         $query = 'SELECT email, adaid FROM ada';
         $stmt = $pdo->query($query);
+        assertType('PDOStatement<array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}'. $bothType .'>', $stmt);
 
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>}'. $bothType .'>', $stmt);
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        assertType('array{email: string, adaid: int<0, 4294967295>}', $result);
+    }
+
+    public function setFetchModeOnQuery(PDO $pdo)
+    {
+        $bothType = ', array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}';
+
+        $query = 'SELECT email, adaid FROM ada';
+        $stmt = $pdo->query($query, PDO::FETCH_NUM);
+        assertType('PDOStatement<array{string, int<0, 4294967295>}'. $bothType .'>', $stmt);
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>}'. $bothType .'>', $stmt);
+
+        $result = $stmt->fetch(PDO::FETCH_NUM);
+        assertType('array{string, int<0, 4294967295>}', $result);
     }
 }
