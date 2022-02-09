c1ef8e9eb1dd0cd181e8995541eb520199088620
Markus Staab <maggus.staab@googlemail.com>
2022-02-09 21:37:39 +0100

    Update ReflectorFactory.php

--- tests/ReflectorFactory.php
+++ tests/ReflectorFactory.php
@@ -3,7 +3,9 @@
 namespace staabm\PHPStanDba\Tests;
 
 use mysqli;
+use PDO;
 use staabm\PHPStanDba\QueryReflection\MysqliQueryReflector;
+use staabm\PHPStanDba\QueryReflection\PDOQueryReflector;
 use staabm\PHPStanDba\QueryReflection\QueryReflector;
 use staabm\PHPStanDba\QueryReflection\RecordingQueryReflector;
 use staabm\PHPStanDba\QueryReflection\ReflectionCache;
@@ -29,8 +31,11 @@ final class ReflectorFactory
         $reflector = getenv('DBA_REFLECTOR') ?: 'mysqli';
         if ('recording' === $mode) {
             if ('mysqli' === $reflector) {
-                $mysqli = @new mysqli($host, $user, $password, $dbname);
+                $mysqli = new mysqli($host, $user, $password, $dbname);
                 $reflector = new MysqliQueryReflector($mysqli);
+            } elseif ('pdo' === $reflector) {
+                $pdo = new PDO(sprintf('mysql:dbname=%s;host=%s', $dbname, $host), $user, $password);
+                $reflector = new PDOQueryReflector($pdo);
             } else {
                 throw new \RuntimeException('Unknown reflector: '.$reflector);
             }
