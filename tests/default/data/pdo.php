3d2373ff84137bbdbdc01558a629f7017f8ec29e
Markus Staab <m.staab@complex-it.de>
2022-02-09 10:50:11 +0100

    adjust expectations

--- tests/default/data/pdo.php
+++ tests/default/data/pdo.php
@@ -123,6 +123,8 @@ class Foo
 
     public function queryBranches(PDO $pdo, bool $bool, int $adaid)
     {
+        $bothType = ', array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>}';
+
         $query = 'SELECT email, adaid FROM ada';
         $stmt = $pdo->query($query, PDO::FETCH_NUM);
         assertType('PDOStatement<array{string, int<0, 4294967295>}'. $bothType .'>', $stmt);
@@ -144,6 +146,8 @@ class Foo
 
     public function leftJoinQuery(PDO $pdo)
     {
+        $bothType = ', array{email: string, 0: string, adaid: int<0, 4294967295>|null, 1: int<0, 4294967295>|null, gesperrt: int<-128, 127>|null, 2: int<-128, 127>|null}';
+
         $query = 'SELECT a.email, b.adaid, b.gesperrt FROM ada a LEFT JOIN ada b ON a.adaid=b.adaid';
         $stmt = $pdo->query($query, PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>|null, gesperrt: int<-128, 127>|null}'. $bothType .'>', $stmt);
@@ -155,6 +159,8 @@ class Foo
      */
     public function unionParam(PDO $pdo, $adaid, $email)
     {
+        $bothType = ', array{email: string, 0: string, adaid: int<0, 4294967295>, 1: int<0, 4294967295>, gesperrt: int<-128, 127>, 2: int<-128, 127>, freigabe1u1: int<-128, 127>, 3: int<-128, 127>}';
+
         $stmt = $pdo->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada WHERE adaid = '.$adaid, PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<0, 4294967295>, gesperrt: int<-128, 127>, freigabe1u1: int<-128, 127>}'. $bothType .'>', $stmt);
 
@@ -164,12 +170,16 @@ class Foo
 
     public function mysqlTypes(PDO $pdo)
     {
+        $bothType = ', array{pid: int<0, 4294967295>, 0: int<0, 4294967295>, c_char5: string, 1: string, c_varchar255: string, 2: string, c_varchar25: string|null, 3: string|null, c_varbinary255: string, 4: string, c_varbinary25: string|null, 5: string|null, c_date: string|null, 6: string|null, c_time: string|null, 7: string|null, c_datetime: string|null, 8: string|null, c_timestamp: string|null, 9: string|null, c_year: int<0, 255>|null, 10: int<0, 255>|null, c_tiny_text: string|null, 11: string|null, c_medium_text: string|null, 12: string|null, c_text: string|null, 13: string|null, c_long_text: string|null, 14: string|null, c_enum: string, 15: string, c_set: string, 16: string, c_bit: int|null, 17: int|null, c_int: int<-2147483648, 2147483647>, 18: int<-2147483648, 2147483647>, c_tinyint: int<-128, 127>, 19: int<-128, 127>, c_smallint: int<-32768, 32767>, 20: int<-32768, 32767>, c_mediumint: int<-8388608, 8388607>, 21: int<-8388608, 8388607>, c_bigint: int, 22: int, c_double: int, 23: int, c_real: int, 24: int, c_boolean: int<-128, 127>, 25: int<-128, 127>, c_blob: string, 26: string, c_tinyblob: string, 27: string, c_mediumblog: string, 28: string, c_longblob: string, 29: string, c_unsigned_tinyint: int<0, 255>, 30: int<0, 255>, c_unsigned_int: int<0, 4294967295>, 31: int<0, 4294967295>, c_unsigned_smallint: int<0, 65535>, 32: int<0, 65535>, c_unsigned_mediumint: int<0, 16777215>, 33: int<0, 16777215>, c_unsigned_bigint: int<0, max>, 34: int<0, max>}';
+
         $stmt = $pdo->query('SELECT * FROM typemix', PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{pid: int<0, 4294967295>, c_char5: string, c_varchar255: string, c_varchar25: string|null, c_varbinary255: string, c_varbinary25: string|null, c_date: string|null, c_time: string|null, c_datetime: string|null, c_timestamp: string|null, c_year: int<0, 255>|null, c_tiny_text: string|null, c_medium_text: string|null, c_text: string|null, c_long_text: string|null, c_enum: string, c_set: string, c_bit: int|null, c_int: int<-2147483648, 2147483647>, c_tinyint: int<-128, 127>, c_smallint: int<-32768, 32767>, c_mediumint: int<-8388608, 8388607>, c_bigint: int, c_double: int, c_real: int, c_boolean: int<-128, 127>, c_blob: string, c_tinyblob: string, c_mediumblog: string, c_longblob: string, c_unsigned_tinyint: int<0, 255>, c_unsigned_int: int<0, 4294967295>, c_unsigned_smallint: int<0, 65535>, c_unsigned_mediumint: int<0, 16777215>, c_unsigned_bigint: int<0, max>}'. $bothType .'>', $stmt);
     }
 
     public function aggregateFunctions(PDO $pdo)
     {
+        $bothType = ', array{MAX(adaid): int<-2147483648, 2147483647>|null, 0: int<-2147483648, 2147483647>|null, MIN(adaid): int<-2147483648, 2147483647>|null, 1: int<-2147483648, 2147483647>|null, COUNT(adaid): int, 2: int, AVG(adaid): float|null, 3: float|null}';
+
         $query = 'SELECT MAX(adaid), MIN(adaid), COUNT(adaid), AVG(adaid) FROM ada WHERE adaid = 1';
         $stmt = $pdo->query($query, PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{MAX(adaid): int<-2147483648, 2147483647>|null, MIN(adaid): int<-2147483648, 2147483647>|null, COUNT(adaid): int, AVG(adaid): float|null}'. $bothType .'>', $stmt);
