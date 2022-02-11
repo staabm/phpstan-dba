3ffc7812e7e087fb126f33f45b94bcb9fa0f84cb
Markus Staab <m.staab@complex-it.de>
2022-02-11 19:04:04 +0100

    fix tests

--- tests/default/Fixture/BaseQueryClass.php
+++ tests/default/Fixture/BaseQueryClass.php
@@ -9,6 +9,3 @@ class BaseQueryClass {
     }
 }
 
-class MySubClass extends BaseQueryClass {
-
-}
--- tests/default/SyntaxErrorInPreparedStatementMethodSubclassedRuleTest.php
+++ tests/default/SyntaxErrorInPreparedStatementMethodSubclassedRuleTest.php
@@ -3,17 +3,18 @@
 namespace staabm\PHPStanDba\Tests;
 
 use PHPStan\Rules\Rule;
+use staabm\PHPStanDba\Rules\SyntaxErrorInPreparedStatementMethodRule;
 use staabm\PHPStanDba\Rules\SyntaxErrorInQueryMethodRule;
 use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
 
 /**
  * @extends AbstractServiceAwareRuleTestCase<SyntaxErrorInQueryMethodRule>
  */
-class SyntaxErrorInQueryMethodSubclassedRuleTest extends AbstractServiceAwareRuleTestCase
+class SyntaxErrorInPreparedStatementMethodSubclassedRuleTest extends AbstractServiceAwareRuleTestCase
 {
     protected function getRule(): Rule
     {
-        return $this->getRuleFromConfig(SyntaxErrorInQueryMethodRule::class, __DIR__.'/config/subclassed-method-rule.neon');
+        return $this->getRuleFromConfig(SyntaxErrorInPreparedStatementMethodRule::class, __DIR__.'/config/subclassed-method-rule.neon');
     }
 
     public function testSyntaxErrorInQueryRule(): void
@@ -29,6 +30,10 @@ class SyntaxErrorInQueryMethodSubclassedRuleTest extends AbstractServiceAwareRul
                 "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (1064).",
                 18,
             ],
+            [
+                "Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'FROM ada LIMIT 0' at line 3 (1064).",
+                20,
+            ]
         ]);
     }
 }
