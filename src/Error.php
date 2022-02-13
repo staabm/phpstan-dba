d41cc447fe3ac5dbb57bdb692f09fcb69a441714
Markus Staab <m.staab@complex-it.de>
2022-02-13 08:30:03 +0100

    de-duplicate reflector Error logic

--- src/Error.php
+++ src/Error.php
@@ -2,8 +2,10 @@
 
 namespace staabm\PHPStanDba;
 
+use PHPStan\Type\ErrorType;
 use staabm\PHPStanDba\QueryReflection\MysqliQueryReflector;
 use staabm\PHPStanDba\QueryReflection\PdoQueryReflector;
+use staabm\PHPStanDba\QueryReflection\QuerySimulation;
 
 /**
  * @phpstan-type ErrorCodes value-of<MysqliQueryReflector::MYSQL_ERROR_CODES>|value-of<PDOQueryReflector::PDO_ERROR_CODES>
@@ -45,6 +47,38 @@ final class Error
     public function asRuleMessage(): string
     {
         return 'Query error: '.$this->getMessage().' ('.$this->getCode().').';
+    }
+
+    /**
+     * @param ErrorCodes $code
+     */
+    static public function forSyntaxError(\Throwable $exception, $code, string $queryString): self
+    {
+        $message = $exception->getMessage();
+
+        // make error string consistent across mysql/mariadb
+        $message = str_replace(' MySQL server', ' MySQL/MariaDB server', $message);
+        $message = str_replace(' MariaDB server', ' MySQL/MariaDB server', $message);
+
+        // to ease debugging, print the error we simulated
+        $simulatedQuery = QuerySimulation::simulate($queryString);
+        $message = $message."\n\nSimulated query: ".$simulatedQuery;
+
+        return new self($message, $code);
+    }
+
+    /**
+     * @param ErrorCodes $code
+     */
+    static public function forException(\Throwable $exception, $code): self
+    {
+        $message = $exception->getMessage();
+
+        // make error string consistent across mysql/mariadb
+        $message = str_replace(' MySQL server', ' MySQL/MariaDB server', $message);
+        $message = str_replace(' MariaDB server', ' MySQL/MariaDB server', $message);
+
+        return new self($message, $code);
     }
 
     /**
