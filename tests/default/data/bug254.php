aa4775a29215fab97986f940d57b3cdc14772baf
Markus Staab <m.staab@complex-it.de>
2022-02-13 09:06:42 +0100

    fix test

--- tests/default/data/bug254.php
+++ tests/default/data/bug254.php
@@ -5,7 +5,7 @@ namespace Bug254;
 use function PHPStan\Testing\assertType;
 
 class Foo {
-    public function noRows(PDO $pdo)
+    public function noRows(\PDO $pdo)
     {
         $stmt = $pdo->query('SELECT email, adaid FROM ada');
         while ($row = $stmt->fetch()) {
