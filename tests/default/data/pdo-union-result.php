84a28c456c6a9586b03e9c912d44d1d98417a078
Markus Staab <maggus.staab@googlemail.com>
2022-02-14 20:02:15 +0100

    union query `PDO->query()` test coverage (#293)
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- tests/default/data/pdo-union-result.php
+++ tests/default/data/pdo-union-result.php
@@ -13,8 +13,20 @@ class Foo
 
         foreach ($queries as $query) {
             $stmt = $pdo->prepare($query);
+            $stmt->execute([]);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
             assertType('array{adaid: int<0, 4294967295>}|array{email: string}|false', $result);
+        }
+    }
+
+    public function doBar(PDO $pdo)
+    {
+        $queries = ['SELECT adaid FROM ada', 'SELECT email FROM ada'];
+
+        foreach ($queries as $query) {
+            $stmt = $pdo->query($query);
+            $result = $stmt->fetch(PDO::FETCH_NUM);
+            assertType('array{int<0, 4294967295>|string}|false', $result);
         }
     }
 }
