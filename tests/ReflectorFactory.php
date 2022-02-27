8e3eb09604c25c30fbfdeec72cc72a3846dd79fe
Pavel Karfik <pavel.karfik@cdn77.com>
2022-02-27 11:20:43 +0100

    separate phpunit cache files for pgsql

--- tests/ReflectorFactory.php
+++ tests/ReflectorFactory.php
@@ -42,7 +42,11 @@ final class ReflectorFactory
         // we need to record the reflection information in both, phpunit and phpstan since we are replaying it in both CI jobs.
         // in a regular application you will use phpstan-dba only within your phpstan CI job, therefore you only need 1 cache-file.
         // phpstan-dba itself is a special case, since we are testing phpstan-dba with phpstan-dba.
-        $cacheFile = $cacheDir.'/.phpunit-phpstan-dba-'.$reflector.'.cache';
+        $cacheFile = sprintf(
+            '%s/.phpunit-phpstan-dba-%s.cache',
+            $cacheDir,
+            'pdo' === $reflector ? $dsn : $reflector,
+        );
         if (\defined('__PHPSTAN_RUNNING__')) {
             $cacheFile = $cacheDir.'/.phpstan-dba-'.$reflector.'.cache';
         }
