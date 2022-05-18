8e76959ff1e5f0c03912b9f106181d1bde8b6d78
Markus Staab <maggus.staab@googlemail.com>
2022-05-18 15:54:42 +0200

    Update pdo-pgsql.php

--- tests/default/data/pdo-pgsql.php
+++ tests/default/data/pdo-pgsql.php
@@ -7,7 +7,7 @@ use function PHPStan\Testing\assertType;
 
 class Foo
 {
-    public function mysqlTypes(PDO $pdo)
+    public function pgsqlTypes(PDO $pdo)
     {
         $stmt = $pdo->query('SELECT * FROM typemix', PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{pid: int<1, 2147483647>, c_varchar5: string, c_varchar25: string|null, c_varchar255: string, c_date: string|null, c_time: string|null, c_datetime: string|null, c_timestamp: string|null, c_text: string|null, c_enum: mixed, c_bit255: int, c_bit25: int|null, c_bit: int|null, c_int: int<-2147483648, 2147483647>, c_smallint: int<-32768, 32767>, c_bigint: int, c_float: float, c_boolean: mixed}>', $stmt);
