cfe0d7c5b907743c3c21c699f78ac009f056b3cf
Markus Staab <m.staab@complex-it.de>
2022-05-23 12:50:06 +0200

    re-record

--- tests/default/data/bug372.php
+++ tests/default/data/bug372.php
@@ -8,12 +8,12 @@ class Foo
 {
     public function differentLineDelims(\PDO $pdo): void
     {
-        $stmt = $pdo->query('SELECT email '. "\r\n" .'FROM ada');
+        $stmt = $pdo->query('SELECT email '."\r\n".'FROM ada');
         while ($row = $stmt->fetch()) {
             assertType('string', $row['email']);
         }
 
-        $stmt = $pdo->query('SELECT adaid '. "\n" .'FROM ada');
+        $stmt = $pdo->query('SELECT adaid '."\n".'FROM ada');
         while ($row = $stmt->fetch()) {
             assertType('int<-32768, 32767>', $row['adaid']);
         }
