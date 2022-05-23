08aa4eae345df24d6b1c592b9614e5b4134d3322
Markus Staab <m.staab@complex-it.de>
2022-05-23 12:33:12 +0200

    reproduce bug 372

---
+++ tests/default/data/bug372.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bug372;
+
+use function PHPStan\Testing\assertType;
+
+class Foo
+{
+    public function differentLineDelims(\PDO $pdo): void
+    {
+        $stmt = $pdo->query('SELECT email '. "\r\n" .'FROM ada');
+        while ($row = $stmt->fetch()) {
+            assertType('string', $row['email']);
+        }
+
+        $stmt = $pdo->query('SELECT adaid '. "\n" .'FROM ada');
+        while ($row = $stmt->fetch()) {
+            assertType('string', $row['email']);
+        }
+    }
+}
