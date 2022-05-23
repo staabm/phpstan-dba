0eeb533743cf8c9f853f83a541dda044582646b2
Markus Staab <m.staab@complex-it.de>
2022-05-23 09:47:31 +0200

    Update PdoQueryReflector.php

--- src/QueryReflection/PdoQueryReflector.php
+++ src/QueryReflection/PdoQueryReflector.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace staabm\PHPStanDba\QueryReflection;
 
+use PDO;
+
 /**
  * This class was kept for BC reasons.
  *
  * @deprected use PdoMysqlQueryReflector instead
  */
 class PdoQueryReflector extends PdoMysqlQueryReflector // @phpstan-ignore-line
-{}
+{
+    public function __construct(PDO $pdo)
+    {
+        parent::__construct($pdo);
+    }
+}
