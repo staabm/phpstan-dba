73141c30580774e58bb3558e4f07bd50e5eb6313
Markus Staab <m.staab@complex-it.de>
2022-05-23 11:32:14 +0200

    added failling test

---
+++ tests/default/data/bug372.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bug372;
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+    public function sayHello(PDO $pdo, string $q): void
+    {
+        $stmt = $pdo->query($q);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('array', $row);
+        }
+    }
+}
