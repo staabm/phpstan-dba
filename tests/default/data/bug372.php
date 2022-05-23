1174d69d1d456061e1ac7667b62b75132d2aa958
Markus Staab <m.staab@complex-it.de>
2022-05-23 11:54:19 +0200

    Update bug372.php

--- tests/default/data/bug372.php
+++ tests/default/data/bug372.php
@@ -6,12 +6,68 @@ use function PHPStan\Testing\assertType;
 
 class HelloWorld
 {
-    public function sayHello(\PDO $pdo, string $q): void
+    public function doFoo(\PDO $pdo): void {
+        $stmt = $pdo->query('SELECT email, adaid FROM ada');
+        assertType('PDOStatement', $stmt);
+
+        foreach ($stmt as $row) {
+            assertType('array{email: string, adaid: int<-32768, 32767>}', $row);
+
+        }
+    }
+
+    public function defaultFetchType(\PDO $pdo, string $q): void
     {
         $stmt = $pdo->query($q);
         assertType('PDOStatement', $stmt);
         foreach($stmt as $row) {
             assertType('array', $row);
         }
+    }
+
+    public function specifiedFetchTypes(\PDO $pdo, string $q): void
+    {
+        $stmt = $pdo->query($q, PDO::FETCH_CLASS);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('stdClass', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_OBJ);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('stdClass', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_KEY_PAIR);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('array{mixed, mixed}', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_ASSOC);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('array<string, mixed>', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_NUM);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('list<mixed>', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_BOTH);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('array<array-key, mixed>', $row);
+        }
+
+        $stmt = $pdo->query($q, PDO::FETCH_COLUMN);
+        assertType('PDOStatement', $stmt);
+        foreach($stmt as $row) {
+            assertType('list<mixed>', $row);
+        }
+
     }
 }
