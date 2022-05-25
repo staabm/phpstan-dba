0b640015a46917b00d17f33993535d278d487eee
Markus Staab <m.staab@complex-it.de>
2022-05-25 21:12:43 +0200

    test expectations

--- tests/rules/SyntaxErrorInPreparedStatementMethodRuleReflectorTest.php
+++ tests/rules/SyntaxErrorInPreparedStatementMethodRuleReflectorTest.php
@@ -75,39 +75,59 @@ class SyntaxErrorInPreparedStatementMethodRuleReflectorTest extends RuleTestCase
         } elseif ('pdo-mysql' === getenv('DBA_REFLECTOR')) {
             $expectedErrors = [
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "freigabe1u1"
+LINE 1: SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada LIMIT...
+                                          ^ (42601).',
                     12,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "freigabe1u1"
+LINE 1: SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada LIMIT...
+                                          ^ (42601).',
                     17,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'FROM ada LIMIT 0' at line 3 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "FROM"
+LINE 3:             FROM ada LIMIT 0
+                    ^ (42601).',
                     22,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'FROM ada LIMIT 0' at line 3 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "FROM"
+LINE 3:             FROM ada LIMIT 0
+                    ^ (42601).',
                     29,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "freigabe1u1"
+LINE 1: SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada LIMIT...
+                                          ^ (42601).',
                     105,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "freigabe1u1"
+LINE 1: SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada LIMIT...
+                                          ^ (42601).',
                     106,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "freigabe1u1"
+LINE 1: SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada LIMIT...
+                                          ^ (42601).',
                     107,
                 ],
                 [
-                    "Query error: SQLSTATE[42S22]: Column not found: 1054 Unknown column 'asdsa' in 'where clause' (42S22).",
+                    'Query error: SQLSTATE[42703]: Undefined column: 7 ERROR:  column "asdsa" does not exist
+LINE 1: ...mail, adaid, gesperrt, freigabe1u1 FROM ada WHERE asdsa=\'1\' ...
+                                                             ^ (42703).',
                     122,
                 ],
                 [
-                    'Value :gesperrt is given, but the query does not contain this placeholder.',
+                    'Value :gesperrt is given, but the query does not contain this placeholder.
+162: Query error: SQLSTATE[42703]: Undefined column: 7 ERROR:  column ":gesperrt%" does not exist
+LINE 1: SELECT adaid FROM ada WHERE email LIKE ":gesperrt%" LIMIT 0
+                                               ^
+HINT:  Perhaps you meant to reference the column "ada.gesperrt". (42703).',
                     137,
                 ],
                 [
@@ -115,7 +135,9 @@ class SyntaxErrorInPreparedStatementMethodRuleReflectorTest extends RuleTestCase
                     307,
                 ],
                 [
-                    "Query error: SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL/MariaDB server version for the right syntax to use near 'gesperrt freigabe1u1 FROM ada LIMIT 0' at line 1 (42000).",
+                    'Query error: SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near "gesperrt"
+LINE 1: SELECT email adaid gesperrt freigabe1u1 FROM ada LIMIT 0
+                           ^ (42601).',
                     319,
                 ],
             ];
