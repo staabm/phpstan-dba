e1551a4aa1547a37825d5e0aa1c442a01790b67b
Markus Staab <maggus.staab@googlemail.com>
2022-05-31 16:13:32 +0200

    support debugMode() in query plan analysis (#398)
    * support debugMode() in query plan analysis
    
    * record
    
    Co-authored-by: Markus Staab <m.staab@complex-it.de>

--- tests/rules/QueryPlanAnalyzerRuleTest.php
+++ tests/rules/QueryPlanAnalyzerRuleTest.php
@@ -20,14 +20,20 @@ class QueryPlanAnalyzerRuleTest extends RuleTestCase
      * @var positive-int
      */
     private $numberOfRowsNotRequiringIndex;
+    /**
+     * @var bool
+     */
+    private $debugMode = false;
 
     protected function tearDown(): void
     {
+        QueryReflection::getRuntimeConfiguration()->debugMode(false);
         QueryReflection::getRuntimeConfiguration()->analyzeQueryPlans(false);
     }
 
     protected function getRule(): Rule
     {
+        QueryReflection::getRuntimeConfiguration()->debugMode($this->debugMode);
         QueryReflection::getRuntimeConfiguration()->analyzeQueryPlans($this->numberOfAllowedUnindexedReads, $this->numberOfRowsNotRequiringIndex);
 
         return self::getContainer()->getByType(QueryPlanAnalyzerRule::class);
@@ -79,6 +85,52 @@ class QueryPlanAnalyzerRuleTest extends RuleTestCase
             ],
             [
                 "Query is not using an index on table 'ada'.".$proposal,
+                28,
+                $tip,
+            ],
+        ]);
+    }
+
+    public function testNotUsingIndexInDebugMode(): void
+    {
+        if ('pdo-pgsql' === getenv('DBA_REFLECTOR')) {
+            $this->markTestSkipped('query plan analyzer is not yet implemented for pgsql');
+        }
+
+        if ('recording' !== getenv('DBA_MODE')) {
+            $this->markTestSkipped('query plan analyzer requires a active database connection');
+        }
+
+        $this->debugMode = true;
+        $this->numberOfAllowedUnindexedReads = true;
+        $this->numberOfRowsNotRequiringIndex = 2;
+
+        $proposal = "\n\nConsider optimizing the query.\nIn some cases this is not a problem and this error should be ignored.";
+        $tip = 'see Mysql Docs https://dev.mysql.com/doc/refman/8.0/en/select-optimization.html';
+
+        $this->analyse([__DIR__.'/data/query-plan-analyzer.php'], [
+            [
+                "Query is not using an index on table 'ada'.".$proposal."\n\nSimulated query: EXPLAIN SELECT * FROM `ada` WHERE email = 'test@example.com'",
+                12,
+                $tip,
+            ],
+            [
+                "Query is not using an index on table 'ada'.".$proposal."\n\nSimulated query: EXPLAIN SELECT *,adaid FROM `ada` WHERE email = 'test@example.com'",
+                17,
+                $tip,
+            ],
+            [
+                "Query is not using an index on table 'ada'.".$proposal."\n\nSimulated query: EXPLAIN SELECT * FROM ada WHERE email = '1970-01-01'",
+                22,
+                $tip,
+            ],
+            [
+                "Query is not using an index on table 'ada'.".$proposal."\n\nSimulated query: EXPLAIN SELECT * FROM ada WHERE email = '1970-01-01'",
+                23,
+                $tip,
+            ],
+            [
+                "Query is not using an index on table 'ada'.".$proposal."\n\nSimulated query: EXPLAIN SELECT * FROM ada WHERE email = '1970-01-01'",
                 28,
                 $tip,
             ],
