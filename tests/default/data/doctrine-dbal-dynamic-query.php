df6ff1c438abe497cac4c9d704883a327476029b
Markus Staab <m.staab@complex-it.de>
2022-07-18 18:23:28 +0200

    added failling test

---
+++ tests/default/data/doctrine-dbal-dynamic-query.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DoctrineDbalDynamicQueryTest;
+
+use Doctrine\DBAL\Connection;
+use function PHPStan\Testing\assertType;
+
+class Foo {
+    public function fetchOneWithDynamicQueryPart(Connection $conn)
+    {
+        $query = 'SELECT email, adaid FROM ada WHERE email != "" AND '.$this->dynamicWhere(rand(0,100));
+        $fetchResult = $conn->fetchOne($query, []);
+        assertType('string|false', $fetchResult);
+    }
+
+    /**
+     * simulating a dynamic where part, not relevant for the query overall result
+     */
+    private function dynamicWhere(int $i) {
+        $where = ['1=1'];
+
+        if ($i > 100) {
+            $where[] = 'adaid = '.$i.'';
+        }
+
+        return implode(' AND ', $where);
+    }
+}
