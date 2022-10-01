537e144aaffb597db1faa61cc590943e0e0aba02
Markus Staab <maggus.staab@googlemail.com>
2022-10-01 16:56:21 +0200

    added failling test for static call with `@phpstandba-inference-placeholder`

--- tests/default/data/inference-placeholder.php
+++ tests/default/data/inference-placeholder.php
@@ -12,6 +12,10 @@ class Foo
         $query = 'SELECT email, adaid FROM ada WHERE email = :email AND '.$this->dynamicWhere(rand(0, 100));
         $fetchResult = $conn->fetchOne($query, ['email' => $email]);
         assertType('string|false', $fetchResult);
+
+        $query = 'SELECT email, adaid FROM ada WHERE email = :email AND '.self::dynamicWhere(rand(0, 100));
+        $fetchResult = $conn->fetchOne($query, ['email' => $email]);
+        assertType('string|false', $fetchResult);
     }
 
     /**
@@ -22,6 +26,24 @@ class Foo
      * @return string
      */
     private function dynamicWhere(int $i)
+    {
+        $where = ['1=1'];
+
+        if ($i > 100) {
+            $where[] = 'adaid = '.$i.'';
+        }
+
+        return implode(' AND ', $where);
+    }
+
+    /**
+     * simulating a dynamic where part, not relevant for the query overall result.
+     *
+     * @phpstandba-inference-placeholder '1=1'
+     *
+     * @return string
+     */
+    static private function staticDynamicWhere(int $i)
     {
         $where = ['1=1'];
 
