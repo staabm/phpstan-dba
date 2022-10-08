5af6bf5ca0bb540cc5dea066082fd90d056cbacc
Markus Staab <maggus.staab@googlemail.com>
2022-10-08 11:28:33 +0200

    cs

--- tests/default/Fixture/Escaper.php
+++ tests/default/Fixture/Escaper.php
@@ -2,22 +2,19 @@
 
 namespace staabm\PHPStanDba\Tests\Fixture;
 
-final class Escaper {
+final class Escaper
+{
     /**
      * @psalm-taint-escape sql
-     *
-     * @return string
      */
-    public function escape($s):string {
-
+    public function escape($s): string
+    {
     }
 
     /**
      * @psalm-taint-escape sql
-     *
-     * @return string
      */
-    static public function staticEscape($s):string {
-
+    public static function staticEscape($s): string
+    {
     }
 }
--- tests/default/data/pdo.php
+++ tests/default/data/pdo.php
@@ -3,8 +3,8 @@
 namespace PdoTest;
 
 use PDO;
-use staabm\PHPStanDba\Tests\Fixture\Escaper;
 use function PHPStan\Testing\assertType;
+use staabm\PHPStanDba\Tests\Fixture\Escaper;
 
 class Foo
 {
@@ -228,14 +228,14 @@ class Foo
 
     public function taintStaticEscaped(PDO $pdo, string $s)
     {
-        $stmt = $pdo->query("SELECT email, adaid FROM ada WHERE adaid=". Escaper::staticEscape($s), PDO::FETCH_ASSOC);
+        $stmt = $pdo->query('SELECT email, adaid FROM ada WHERE adaid='.Escaper::staticEscape($s), PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<-32768, 32767>}>', $stmt);
     }
 
     public function taintEscaped(PDO $pdo, string $s)
     {
         $escapeer = new Escaper();
-        $stmt = $pdo->query("SELECT email, adaid FROM ada WHERE adaid=". $escapeer->escape($s), PDO::FETCH_ASSOC);
+        $stmt = $pdo->query('SELECT email, adaid FROM ada WHERE adaid='.$escapeer->escape($s), PDO::FETCH_ASSOC);
         assertType('PDOStatement<array{email: string, adaid: int<-32768, 32767>}>', $stmt);
     }
 }
