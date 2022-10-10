37c413d8a289f41aa265851a85befdf258d895f0
Markus Staab <m.staab@complex-it.de>
2022-10-10 14:31:11 +0200

    Update pdo-prepare.php

--- tests/default/data/pdo-prepare.php
+++ tests/default/data/pdo-prepare.php
@@ -167,7 +167,7 @@ class Foo
     }
 
     /**
-     * @param list<int> $idsToUpdate
+     * @param list<positive-int> $idsToUpdate
      */
     public function specifiedList(PDO $pdo, array $idsToUpdate, string $time)
     {
