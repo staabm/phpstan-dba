116c859dae13521457aaa0f79f96e7bff7365e6a
Jakub Vojacek <jakub@motv.eu>
2022-11-13 10:28:59 +0100

    WIP

---
+++ src/Extensions/DibiConnectionFetchDynamicReturnTypeExtension.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\Extensions;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\NullType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use staabm\PHPStanDba\QueryReflection\QueryReflection;
+use staabm\PHPStanDba\QueryReflection\QueryReflector;
+use staabm\PHPStanDba\UnresolvableQueryException;
+
+final class DibiConnectionFetchDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return \Dibi\Connection::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return \in_array(strtolower($methodReflection->getName()), ['fetch'], true);
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        $args = $methodCall->getArgs();
+        $defaultReturn = ParametersAcceptorSelector::selectFromArgs(
+            $scope,
+            $methodCall->getArgs(),
+            $methodReflection->getVariants(),
+        )->getReturnType();
+
+        if (\count($args) < 1) {
+            return $defaultReturn;
+        }
+
+        if ($scope->getType($args[0]->value) instanceof MixedType) {
+            return $defaultReturn;
+        }
+
+        try {
+            $resultType = $this->inferType($args[0]->value, $scope);
+            if (null !== $resultType) {
+                return $resultType;
+            }
+        } catch (UnresolvableQueryException $exception) {
+            // simulation not possible.. use default value
+        }
+
+        return $defaultReturn;
+    }
+
+    private function inferType(Expr $queryExpr, Scope $scope): ?Type
+    {
+        $queryReflection = new QueryReflection();
+        $queryStrings = $queryReflection->resolveQueryStrings($queryExpr, $scope);
+
+        $finalQueryString = $queryStrings->current();
+        $finalQueryString = str_replace('?', '"1"', $finalQueryString);
+
+        $row = $queryReflection->getResultType($finalQueryString, QueryReflector::FETCH_TYPE_BOTH);
+
+        if ($row === null) {
+            return $row;
+        }
+
+        return new UnionType([new NullType(), $row]);
+
+    }
+}
---
+++ src/QueryReflection/DibiQueryReflector.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\QueryReflection;
+
+use Dibi\Connection;
+use Dibi\DriverException;
+use mysqli_sql_exception;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\Type;
+use staabm\PHPStanDba\Error;
+use staabm\PHPStanDba\TypeMapping\MysqliTypeMapper;
+
+final class DibiQueryReflector implements QueryReflector, RecordingReflector
+{
+    private const MYSQL_SYNTAX_ERROR_CODE = 1064;
+    private const MYSQL_UNKNOWN_COLUMN_IN_FIELDLIST = 1054;
+    public const MYSQL_UNKNOWN_TABLE = 1146;
+    public const MYSQL_INCORRECT_TABLE = 1103;
+
+    public const MYSQL_HOST_NOT_FOUND = 2002;
+
+    public const NAME = 'mysqli';
+
+    private const MYSQL_ERROR_CODES = [
+        self::MYSQL_SYNTAX_ERROR_CODE,
+        self::MYSQL_UNKNOWN_COLUMN_IN_FIELDLIST,
+        self::MYSQL_UNKNOWN_TABLE,
+        self::MYSQL_INCORRECT_TABLE,
+    ];
+
+    private const MAX_CACHE_SIZE = 50;
+
+    /** @var array<string, mysqli_sql_exception|list<object>|null> */
+    private $cache = [];
+
+    private Connection $connection;
+
+    /**
+     * @var MysqliTypeMapper
+     */
+    private $typeMapper;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+        // set a sane default.. atm this should not have any impact
+
+        $this->typeMapper = new MysqliTypeMapper();
+    }
+
+    public function validateQueryString(string $queryString): ?Error
+    {
+        $result = $this->simulateQuery($queryString);
+        if (!$result instanceof DriverException) {
+            return null;
+        }
+        $e = $result;
+
+        if (\in_array($e->getCode(), self::MYSQL_ERROR_CODES, true)) {
+            if (
+                self::MYSQL_SYNTAX_ERROR_CODE === $e->getCode()
+                && QueryReflection::getRuntimeConfiguration()->isDebugEnabled()
+            ) {
+                return Error::forSyntaxError($e, $e->getCode(), $queryString);
+            }
+
+            return Error::forException($e, $e->getCode());
+        }
+
+        return null;
+    }
+
+    /**
+     * @param self::FETCH_TYPE* $fetchType
+     */
+    public function getResultType(string $queryString, int $fetchType): ?Type
+    {
+        /** @var array<Dibi\Reflection\Column> $result */
+        $result = $this->simulateQuery($queryString);
+
+        if (!\is_array($result)) {
+            return null;
+        }
+
+        $arrayBuilder = ConstantArrayTypeBuilder::createEmpty();
+
+        $i = 0;
+        foreach ($result as $val) {
+            $arrayBuilder->setOffsetValueType(
+                new ConstantStringType($val->getName()),
+                $this->typeMapper->mapToPHPStanType($val->getVendorInfo('type'), $val->getVendorInfo('flags'), $val->getVendorInfo('length'))
+            );
+            $arrayBuilder->setOffsetValueType(
+                new ConstantIntegerType($i),
+                $this->typeMapper->mapToPHPStanType($val->getVendorInfo('type'), $val->getVendorInfo('flags'), $val->getVendorInfo('length'))
+            );
+            ++$i;
+        }
+
+
+        return $arrayBuilder->getArray();
+    }
+
+    /**
+     * @return DriverException|list<object>|null
+     */
+    private function simulateQuery(string $queryString)
+    {
+        if (\array_key_exists($queryString, $this->cache)) {
+            return $this->cache[$queryString];
+        }
+
+        if (\count($this->cache) > self::MAX_CACHE_SIZE) {
+            // make room for the next element by randomly removing a existing one
+            array_shift($this->cache);
+        }
+
+        $simulatedQuery = QuerySimulation::simulate($queryString);
+
+        if (null === $simulatedQuery) {
+            return $this->cache[$queryString] = null;
+        }
+
+        $this->connection->query('START transaction');
+
+        try {
+            $result = $this->connection->query($simulatedQuery);
+            $resultInfo = $result->getColumns();
+
+            return $this->cache[$queryString] = $resultInfo;
+        } catch (DriverException $e) {
+            return $this->cache[$queryString] = $e;
+        } finally {
+            $this->connection->query('ROLLBACK');
+        }
+    }
+
+    public function getDatasource(): Connection
+    {
+        return $this->connection;
+    }
+}
