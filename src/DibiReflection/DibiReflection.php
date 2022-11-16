389b8e24aa94d402f4ceaaf251437310aabf0c33
Markus Staab <maggus.staab@googlemail.com>
2022-11-16 21:58:52 +0100

    simplify dibi

---
+++ src/DibiReflection/DibiReflection.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\DibiReflection;
+
+use Dibi\Connection;
+use Dibi\ConstraintViolationException;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\Type;
+use staabm\PHPStanDba\Error;
+use staabm\PHPStanDba\TypeMapping\MysqliTypeMapper;
+
+final class DibiReflection
+{
+    public function rewriteQuery(string $queryString): ?string
+    {
+        $queryString = str_replace('%in', '(1)', $queryString);
+        $queryString = str_replace('%lmt', 'LIMIT 1', $queryString);
+        $queryString = str_replace('%ofs', ', 1', $queryString);
+        $queryString = preg_replace('#%(i|s)#', '"1"', $queryString) ?? '';
+        $queryString = preg_replace('#%(t|d)#', '"2000-1-1"', $queryString) ?? '';
+        $queryString = preg_replace('#%(and|or)#', '(1 = 1)', $queryString) ?? '';
+        $queryString = preg_replace('#%~?like~?#', '"%1%"', $queryString) ?? '';
+
+        if (strpos($queryString, '%n') > 0) {
+            $queryString = null;
+        } elseif (strpos($queryString, '%ex') > 0) {
+            $queryString = null;
+        } elseif (0 !== preg_match('#^\s*(START|ROLLBACK|SET|SAVEPOINT|SHOW)#i', $queryString)) {
+            $queryString = null;
+        }
+
+        return $queryString;
+    }
+}
--- src/Extensions/DibiConnectionFetchDynamicReturnTypeExtension.php
+++ src/Extensions/DibiConnectionFetchDynamicReturnTypeExtension.php
@@ -8,15 +8,24 @@ use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
-use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Generic\GenericObjectType;
+use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\MixedType;
 use PHPStan\Type\NullType;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\UnionType;
+use staabm\PHPStanDba\DibiReflection\DibiReflection;
+use staabm\PHPStanDba\DoctrineReflection\DoctrineReflection;
+use staabm\PHPStanDba\DoctrineReflection\DoctrineResultObjectType;
 use staabm\PHPStanDba\QueryReflection\QueryReflection;
 use staabm\PHPStanDba\QueryReflection\QueryReflector;
 use staabm\PHPStanDba\UnresolvableQueryException;
@@ -38,54 +47,152 @@ final class DibiConnectionFetchDynamicReturnTypeExtension implements DynamicMeth
         ], true);
     }
 
-    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
     {
         $args = $methodCall->getArgs();
-        $defaultReturn = ParametersAcceptorSelector::selectFromArgs(
-            $scope,
-            $methodCall->getArgs(),
-            $methodReflection->getVariants(),
-        )->getReturnType();
 
         if (\count($args) < 1) {
-            return $defaultReturn;
+            return null;
         }
 
         if ($scope->getType($args[0]->value) instanceof MixedType) {
-            return $defaultReturn;
+            return null;
         }
 
         try {
-            $resultType = $this->inferType($args[0]->value, $scope, $methodReflection->getName());
-            if (null !== $resultType) {
-                return $resultType;
-            }
+            return $this->inferType($methodReflection, $args[0]->value, $scope);
         } catch (UnresolvableQueryException $exception) {
             // simulation not possible.. use default value
         }
 
-        return $defaultReturn;
+        return null;
     }
 
-    private function inferType(Expr $queryExpr, Scope $scope, string $methodName): ?Type
+    private function inferType(MethodReflection $methodReflection, Expr $queryExpr, Scope $scope): ?Type
     {
         $queryReflection = new QueryReflection();
-        $resolvedQuery = $queryReflection->resolveQueryString($queryExpr, $scope);
+        $queryStrings = $queryReflection->resolveQueryStrings($queryExpr, $scope);
 
-        if (null === $resolvedQuery) {
-            return null;
+        return $this->createFetchType($queryStrings, $methodReflection);
+    }
+
+    /**
+     * @param iterable<string> $queryStrings
+     */
+    private function createFetchType(iterable $queryStrings, MethodReflection $methodReflection): ?Type
+    {
+        $queryReflection = new QueryReflection();
+        $dibiReflection = new DibiReflection();
+
+        $fetchTypes = [];
+        foreach ($queryStrings as $queryString) {
+            $queryString = $dibiReflection->rewriteQuery($queryString);
+            if ($queryString === null) {
+                continue;
+            }
+
+            $resultType = $queryReflection->getResultType($queryString, QueryReflector::FETCH_TYPE_BOTH);
+
+            if (null === $resultType) {
+                return null;
+            }
+
+            $fetchResultType = $this->reduceResultType($methodReflection, $resultType);
+            if (null === $fetchResultType) {
+                return null;
+            }
+
+            $fetchTypes[] = $fetchResultType;
         }
 
-        $result = $queryReflection->getResultType($resolvedQuery, QueryReflector::FETCH_TYPE_COLUMN);
+        if (\count($fetchTypes) > 1) {
+            return TypeCombinator::union(...$fetchTypes);
+        }
+        if (1 === \count($fetchTypes)) {
+            return $fetchTypes[0];
+        }
 
-        if ('fetch' === $methodName && null !== $result) {
-            return new UnionType([new NullType(), $result]);
-        } elseif ('fetchAll' === $methodName && null !== $result) {
-            return new ArrayType(new IntegerType(), $result);
-        } elseif ('fetchPairs' === $methodName && $result instanceof ConstantArrayType && 2 === \count($result->getValueTypes())) {
-            return new ArrayType($result->getValueTypes()[0], $result->getValueTypes()[1]);
-        } elseif ('fetchSingle' === $methodName && $result instanceof ConstantArrayType && 1 === \count($result->getValueTypes())) {
-            return new UnionType([new NullType(), $result->getValueTypes()[0]]);
+        return null;
+    }
+
+    private function reduceResultType(MethodReflection $methodReflection, Type $resultType): ?Type
+    {
+        if ($resultType instanceof UnionType) {
+            $resultTypes = [];
+
+            foreach ($resultType->getTypes() as $type) {
+                $rowType = $this->reduceResultType($methodReflection, $type);
+                if (null === $rowType) {
+                    return null;
+                }
+                $resultTypes[] = $rowType;
+            }
+
+            return TypeCombinator::union(...$resultTypes);
+        }
+
+        $resultRowType = $resultType;
+        $usedMethod = strtolower($methodReflection->getName());
+
+        switch ($usedMethod) {
+            case 'fetchPairs':
+                $fetchType = QueryReflector::FETCH_TYPE_KEY_VALUE;
+                break;
+            case 'fetchSingle':
+                $fetchType = QueryReflector::FETCH_TYPE_ONE;
+                break;
+            case 'fetchnumeric':
+            case 'fetchallnumeric':
+                $fetchType = QueryReflector::FETCH_TYPE_NUMERIC;
+                break;
+            case 'fetchAll':
+                $fetchType = QueryReflector::FETCH_TYPE_ASSOC;
+                break;
+            default:
+                $fetchType = QueryReflector::FETCH_TYPE_BOTH;
+        }
+
+        if (QueryReflector::FETCH_TYPE_BOTH !== $fetchType && $resultRowType instanceof ConstantArrayType) {
+            $builder = ConstantArrayTypeBuilder::createEmpty();
+
+            $keyTypes = $resultRowType->getKeyTypes();
+            $valueTypes = $resultRowType->getValueTypes();
+
+            if (QueryReflector::FETCH_TYPE_KEY_VALUE === $fetchType) {
+                // $valueType contain 'BOTH' fetched values
+                if (\count($valueTypes) < 4) {
+                    return null;
+                }
+
+                if (\in_array($usedMethod, ['fetchallkeyvalue'], true)) {
+                    return new ArrayType($valueTypes[0], $valueTypes[2]);
+                }
+
+                return new GenericObjectType(Traversable::class, [$valueTypes[0], $valueTypes[2]]);
+            }
+
+            foreach ($keyTypes as $i => $keyType) {
+                if (QueryReflector::FETCH_TYPE_ONE === $fetchType) {
+                    return TypeCombinator::union($valueTypes[$i], new ConstantBooleanType(false));
+                }
+                if (QueryReflector::FETCH_TYPE_FIRST_COL === $fetchType) {
+                    return new ArrayType(IntegerRangeType::fromInterval(0, null), $valueTypes[$i]);
+                }
+
+                if (QueryReflector::FETCH_TYPE_NUMERIC === $fetchType && $keyType instanceof ConstantIntegerType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                } elseif (QueryReflector::FETCH_TYPE_ASSOC === $fetchType && $keyType instanceof ConstantStringType) {
+                    $builder->setOffsetValueType($keyType, $valueTypes[$i]);
+                }
+            }
+
+            $resultType = $builder->getArray();
+
+            if (\in_array($usedMethod, ['fetchAll'], true)) {
+                return new ArrayType(IntegerRangeType::fromInterval(0, null), $resultType);
+            }
+
+            return $resultType;
         }
 
         return null;
