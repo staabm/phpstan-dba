693882d409ca03b9a03c163b95305a105fb21d60
Jakub Vojacek <jakub@motv.eu>
2022-11-17 21:06:20 +0100

    Added tests

--- tests/rules/data/syntax-error-in-dibi-prepared-statement.php
+++ tests/rules/data/syntax-error-in-dibi-prepared-statement.php
@@ -41,6 +41,13 @@ class Foo
         $conn->fetchPairs('SELECT email FROM ada where email = %s', 'email@github.com');
     }
 
+    public function testIgnoredPlaceholders(\Dibi\Connection $conn)
+    {
+        $conn->query('SELECT %n, %n from %n', 'email', 'dadid', 'ada');
+        $conn->query('SELECT email, adaid FROM ada group by %by', 'email');
+        $conn->query('SELECT email, adaid FROM ada where email = %bin group by %by', 1, 'email');
+    }
+
     /* phpstan-dba does not yet support writable queries
     public function testDeleteUpdateInsert(\Dibi\Connection $conn)
     {
