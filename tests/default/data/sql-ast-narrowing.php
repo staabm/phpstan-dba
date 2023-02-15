9dd2ec1b029d626a292d188bfb0b13ff46993771
Markus Staab <markus.staab@redaxo.de>
2023-02-15 13:41:41 +0100

    Update sql-ast-narrowing.php

--- tests/default/data/sql-ast-narrowing.php
+++ tests/default/data/sql-ast-narrowing.php
@@ -7,7 +7,7 @@ use function PHPStan\Testing\assertType;
 
 class Foo
 {
-    public function count(PDO $pdo)
+    public function count(PDO $pdo): void
     {
         $stmt = $pdo->query('SELECT count(email) as myemail from ada');
         assertType('PDOStatement<array{myemail: int<0, max>, 0: int<0, max>}>', $stmt);
@@ -16,7 +16,7 @@ class Foo
         assertType('PDOStatement<array{myemail: int<0, max>, 0: int<0, max>, count(email): int, 1: int<0, max>}>', $stmt);
     }
 
-    public function coalesce(PDO $pdo)
+    public function coalesce(PDO $pdo): void
     {
         $stmt = $pdo->query('SELECT COALESCE(null, eladaid) as col from ak');
         assertType('PDOStatement<array{col: int<-2147483648, 2147483647>|null, 0: int<-2147483648, 2147483647>|null}>', $stmt);
@@ -25,7 +25,7 @@ class Foo
         assertType('PDOStatement<array{col: int<-2147483648, 2147483647>, 0: int<-2147483648, 2147483647>}>', $stmt);
     }
 
-    public function ifnull(PDO $pdo)
+    public function ifnull(PDO $pdo): void
     {
         $stmt = $pdo->query('SELECT ifnull(freigabe1u1, 5000) as col from ada');
         assertType('PDOStatement<array{col: 5000|int<-128, 127>, 0: 5000|int<-128, 127>}>', $stmt);
@@ -43,7 +43,7 @@ class Foo
         assertType('PDOStatement<array{col: int<-128, 127>, 0: int<-128, 127>}>', $stmt);
     }
 
-    public function nullif(PDO $pdo)
+    public function nullif(PDO $pdo): void
     {
         $stmt = $pdo->query('SELECT nullif(freigabe1u1, 5000) as col from ada');
         assertType('PDOStatement<array{col: 5000|int<-128, 127>, 0: 5000|int<-128, 127>}>', $stmt);
