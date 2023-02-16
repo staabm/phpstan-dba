b85418ed92fdd1161a847dd42af8b7238e9c98d7
Markus Staab <maggus.staab@googlemail.com>
2023-02-16 22:06:12 +0100

    fix

--- tests/stringify/data/stringify.php
+++ tests/stringify/data/stringify.php
@@ -20,15 +20,3 @@ class Foo
         }
     }
 }
-
-class SqlAstNarrowing
-{
-    public function count(PDO $pdo): void
-    {
-        $stmt = $pdo->query('SELECT count(email) as myemail from ada');
-        assertType('PDOStatement<array{myemail: numeric-string, 0: numeric-string}>', $stmt);
-
-        $stmt = $pdo->query('SELECT count(email) as myemail, count(email) from ada');
-        assertType('PDOStatement<array{myemail: numeric-string, 0: numeric-string, count(email): numeric-string, 1: numeric-string}>', $stmt);
-    }
-}
