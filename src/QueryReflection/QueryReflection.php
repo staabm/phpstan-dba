680d928be68a9eddbce3687ceac9952c711d1f57
Markus Staab <maggus.staab@googlemail.com>
2023-02-26 15:47:28 +0100

    Restore src/QueryReflection/QueryReflection.php

--- src/QueryReflection/QueryReflection.php
+++ src/QueryReflection/QueryReflection.php
@@ -32,7 +32,6 @@ use staabm\PHPStanDba\PhpDoc\PhpDocUtil;
 use staabm\PHPStanDba\SchemaReflection\SchemaReflection;
 use staabm\PHPStanDba\SqlAst\ParserInference;
 use staabm\PHPStanDba\UnresolvableQueryException;
-use staabm\PHPStanDba\UnresolvableQueryInvalidAfterSimulationException;
 
 final class QueryReflection
 {
@@ -298,10 +297,6 @@ final class QueryReflection
             $error = $this->validateQueryString($normalizedQuery);
             if ($error === null) {
                 yield $normalizedQuery;
-            } else {
-                if (QueryReflection::getRuntimeConfiguration()->isDebugEnabled()) {
-                    throw new UnresolvableQueryInvalidAfterSimulationException('Seems the query is too dynamic to be resolved by query simulation');
-                }
             }
         }
     }
