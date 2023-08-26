dd34c98a11696347eee4a179c966d0cd0cd2c591
Thomas Bley <mails@thomasbley.de>
2023-08-26 17:59:25 +0200

    #603 cleanup

--- tests/rules/data/bug-603.php
+++ tests/rules/data/bug-603.php
@@ -4,12 +4,9 @@ namespace Bug603;
 
 use PDO;
 
-function taintEscapedAndInferencePlaceholder(PDO $pdo, string $s, int $start, int $max)
+function preparedStatementWithBindOnExecute(PDO $pdo)
 {
     $statement = $pdo->prepare('SELECT * FROM invalid_table WHERE id = ?');
     $statement->execute([123]);
     $statement->fetch();
-}
-
-class X {
 }
