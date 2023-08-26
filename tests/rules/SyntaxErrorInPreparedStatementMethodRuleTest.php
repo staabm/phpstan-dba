c1209be87192cbe89f3adeef98b96782e8412316
Thomas Bley <mails@thomasbley.de>
2023-08-26 18:15:55 +0200

    #603 enable mysqli error message

--- tests/rules/SyntaxErrorInPreparedStatementMethodRuleTest.php
+++ tests/rules/SyntaxErrorInPreparedStatementMethodRuleTest.php
@@ -268,7 +268,14 @@ LINE 1: EXPLAIN INSERT IGNORE INTO `s_articles_supplier` (`id`, `nam...
         }
 
         if (MysqliQueryReflector::NAME === getenv('DBA_REFLECTOR')) {
-            self::markTestSkipped('Error message different depending on version of the database.');
+            $expectedErrors = [
+                [
+                    'Query error: SQLSTATE[42P01]: Undefined table: 7 ERROR:  relation "invalid_table" does not exist
+LINE 1: SELECT * FROM invalid_table WHERE id = \'123\' LIMIT 0
+                      ^ (42P01).',
+                    10,
+                ],
+            ];
         } elseif (PdoPgSqlQueryReflector::NAME === getenv('DBA_REFLECTOR')) {
             $expectedErrors = [
                 [
