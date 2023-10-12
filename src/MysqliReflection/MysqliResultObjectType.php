93ff6a961106eafd65609a1fff3193a58a2160b5
Markus Staab <markus.staab@redaxo.de>
2023-10-12 10:38:00 +0200

    Refactor MysqliResultObjectType

--- src/MysqliReflection/MysqliResultObjectType.php
+++ src/MysqliReflection/MysqliResultObjectType.php
@@ -7,11 +7,46 @@ namespace staabm\PHPStanDba\MysqliReflection;
 use mysqli_result;
 use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\Type;
+use PHPStan\TrinaryLogic;
 
 final class MysqliResultObjectType extends GenericObjectType
 {
-    public function __construct(Type $rowType)
+    /**
+     * @param array<int, Type> $types
+     */
+    public function __construct(private array $types, ?Type $subtractedType = null)
     {
-        parent::__construct(mysqli_result::class, [$rowType]);
+        parent::__construct(mysqli_result::class, $types, $subtractedType);
     }
+
+    public function getTypeWithoutSubtractedType(): Type
+    {
+        $withoutSubtracted = $this->changeSubtractedType(null);
+        if ($withoutSubtracted instanceof GenericObjectType) {
+            return new self(
+                $withoutSubtracted->getTypes(),
+                $withoutSubtracted->getSubtractedType(),
+            );
+        }
+        return $withoutSubtracted;
+    }
+
+    public function equals(Type $type): bool
+    {
+        if ($type instanceof self) {
+            return false;
+        }
+
+        return parent::equals($type);
+    }
+
+    public function isSuperTypeOf(Type $type): TrinaryLogic
+    {
+        if ($type instanceof self) {
+            return TrinaryLogic::createFromBoolean($this->equals($type));
+        }
+
+        return parent::isSuperTypeOf($type);
+    }
+
 }
