d94879e9676cb6606746997fb405bcf953dd8693
Markus Staab <maggus.staab@googlemail.com>
2024-01-22 17:20:46 +0100

    Discard changes to tests/default/DbaInferenceTest.php

--- tests/default/DbaInferenceTest.php
+++ tests/default/DbaInferenceTest.php
@@ -85,7 +85,6 @@ class DbaInferenceTest extends TypeInferenceTestCase
     public static function getAdditionalConfigFiles(): array
     {
         return [
-            'phar://phpstan.phar/conf/bleedingEdge.neon',
             __DIR__ . '/../../config/stubFiles.neon',
             __DIR__ . '/../../config/extensions.neon',
         ];
