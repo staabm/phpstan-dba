81c37cf5a3486e692fe3ae583f7296931423f527
Markus Staab <maggus.staab@googlemail.com>
2024-10-28 15:30:14 +0100

    Update bug-680.php

--- tests/default/data/bug-680.php
+++ tests/default/data/bug-680.php
@@ -15,9 +15,9 @@ class Test
             ->connection
             ->fetchAssociative(
                 '#cart-persister::load
-                SELECT email, adaid FROM ada WHERE adaid = :token',
+                SELECT email FROM ada WHERE adaid = :token',
                 ['token' => $token],
             );
-        assertType('array{email: string, adaid: int<-32768, 32767>}|false', $content);
+        assertType('array{email: string}|false', $content);
     }
 }
