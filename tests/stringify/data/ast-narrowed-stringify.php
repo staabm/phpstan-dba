ea2919fa2f989f78b8c152091759bd3fae36e5a6
Markus Staab <markus.staab@redaxo.de>
2024-11-07 15:58:43 +0100

    fix

--- tests/stringify/data/ast-narrowed-stringify.php
+++ tests/stringify/data/ast-narrowed-stringify.php
@@ -10,9 +10,13 @@ class SqlAstNarrowing
     public function count(PDO $pdo): void
     {
         $stmt = $pdo->query('SELECT count(email) as myemail from ada');
-        assertType('PDOStatement<array{myemail: numeric-string, 0: numeric-string}>', $stmt);
+        foreach ($stmt as $row) {
+            assertType("array{myemail: numeric-string, 0: numeric-string}", $row);
+        }
 
         $stmt = $pdo->query('SELECT count(email) as myemail, count(email) from ada');
-        assertType('PDOStatement<array{myemail: numeric-string, 0: numeric-string, count(email): numeric-string, 1: numeric-string}>', $stmt);
+        foreach ($stmt as $row) {
+            assertType("array{myemail: numeric-string, 0: numeric-string, count(email): numeric-string, 1: numeric-string}", $row);
+        }
     }
 }
--- tests/stringify/data/stringify.php
+++ tests/stringify/data/stringify.php
@@ -10,9 +10,8 @@ class Foo
     public function stringifyTypes(PDO $pdo)
     {
         $stmt = $pdo->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada', PDO::FETCH_ASSOC);
-        assertType('PDOStatement<array{email: string, adaid: numeric-string, gesperrt: numeric-string, freigabe1u1: numeric-string}>', $stmt);
-
         foreach ($stmt as $row) {
+            assertType("array{email: string, adaid: numeric-string, gesperrt: numeric-string, freigabe1u1: numeric-string}", $row);
             assertType('numeric-string', $row['adaid']);
             assertType('string', $row['email']);
             assertType('numeric-string', $row['gesperrt']);
