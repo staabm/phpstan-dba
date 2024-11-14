5b1f30d6ad32449abe4761ae24f4341fc3ff6bdc
Markus Staab <maggus.staab@googlemail.com>
2024-11-14 13:08:30 +0100

    Simplify a phpstan repro

--- src/SqlAst/ParserInference.php
+++ src/SqlAst/ParserInference.php
@@ -1,14 +1,9 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace staabm\PHPStanDba\SqlAst;
 
 use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\Constant\ConstantArrayType;
-use PHPStan\Type\Constant\ConstantIntegerType;
-use PHPStan\Type\Constant\ConstantStringType;
-use PHPStan\Type\MixedType;
 use PHPStan\Type\Type;
 use SqlFtw\Parser\Parser;
 use SqlFtw\Parser\ParserConfig;
@@ -16,29 +11,14 @@ use SqlFtw\Platform\Platform;
 use SqlFtw\Session\Session;
 use SqlFtw\Sql\Dml\Query\SelectCommand;
 use SqlFtw\Sql\Dml\Query\SelectExpression;
-use SqlFtw\Sql\Dml\TableReference\InnerJoin;
 use SqlFtw\Sql\Dml\TableReference\Join;
-use SqlFtw\Sql\Dml\TableReference\TableReferenceSubquery;
 use SqlFtw\Sql\Dml\TableReference\TableReferenceTable;
-use SqlFtw\Sql\Expression\Asterisk;
 use SqlFtw\Sql\Expression\Identifier;
-use SqlFtw\Sql\Expression\SimpleName;
 use SqlFtw\Sql\SqlSerializable;
-use staabm\PHPStanDba\QueryReflection\QueryReflection;
-use staabm\PHPStanDba\SchemaReflection\Join as SchemaJoin;
-use staabm\PHPStanDba\SchemaReflection\SchemaReflection;
 use staabm\PHPStanDba\SchemaReflection\Table;
-use staabm\PHPStanDba\UnresolvableAstInQueryException;
 
 final class ParserInference
 {
-    private SchemaReflection $schemaReflection;
-
-    public function __construct(SchemaReflection $schemaReflection)
-    {
-        $this->schemaReflection = $schemaReflection;
-    }
-
     public function narrowResultType(string $queryString, ConstantArrayType $resultType): Type
     {
         $platform = Platform::get(Platform::MYSQL, '8.0'); // version defaults to x.x.99 when no patch number is given
@@ -53,9 +33,6 @@ final class ParserInference
 
         $selectColumns = null;
         $fromTable = null;
-        $where = null;
-        $groupBy = null;
-        $joins = [];
         foreach ($commands as $command) {
             // Parser does not throw exceptions. this allows to parse partially invalid code and not fail on first error
             if ($command instanceof SelectCommand) {
@@ -63,64 +40,16 @@ final class ParserInference
                     $selectColumns = $command->getColumns();
                 }
                 $from = $command->getFrom();
-                $where = $command->getWhere();
-                $groupBy = $command->getGroupBy();
 
                 if (null === $from) {
                     // no FROM clause, use an empty Table to signify this
                     $fromTable = new Table('', []);
                 } elseif ($from instanceof TableReferenceTable) {
                     $fromName = $from->getTable()->getName();
-                    $fromTable = $this->schemaReflection->getTable($fromName);
                 } elseif ($from instanceof Join) {
                     while (1) {
                         if ($from->getCondition() === null) {
-                            if (QueryReflection::getRuntimeConfiguration()->isDebugEnabled()) {
-                                throw new UnresolvableAstInQueryException('Cannot narrow down types null join conditions: ' . $queryString);
-                            }
-
                             return $resultType;
-                        }
-
-                        if ($from->getRight() instanceof TableReferenceSubquery || $from->getLeft() instanceof TableReferenceSubquery) {
-                            if (QueryReflection::getRuntimeConfiguration()->isDebugEnabled()) {
-                                throw new UnresolvableAstInQueryException('Cannot narrow down types for SQLs with subqueries: ' . $queryString);
-                            }
-
-                            return $resultType;
-                        }
-
-                        if ($from instanceof InnerJoin && $from->isCrossJoin()) {
-                            if (QueryReflection::getRuntimeConfiguration()->isDebugEnabled()) {
-                                throw new UnresolvableAstInQueryException('Cannot narrow down types for cross joins: ' . $queryString);
-                            }
-
-                            return $resultType;
-                        }
-
-                        $joinType = SchemaJoin::TYPE_OUTER;
-
-                        if ($from instanceof InnerJoin) {
-                            $joinType = SchemaJoin::TYPE_INNER;
-                        }
-
-                        $joinedTable = $this->schemaReflection->getTable($from->getRight()->getTable()->getName());
-
-                        if ($joinedTable !== null) {
-                            $joins[] = new SchemaJoin(
-                                $joinType,
-                                $joinedTable,
-                                $from->getCondition()
-                            );
-                        }
-
-                        if ($from->getLeft() instanceof TableReferenceTable) {
-                            $from = $from->getLeft();
-
-                            $fromName = $from->getTable()->getName();
-                            $fromTable = $this->schemaReflection->getTable($fromName);
-
-                            break;
                         }
 
                         $from = $from->getLeft();
@@ -133,73 +62,7 @@ final class ParserInference
             // not parsable atm, return un-narrowed type
             return $resultType;
         }
-        if (null === $selectColumns) {
-            throw new ShouldNotHappenException();
-        }
-
-        $queryScope = new QueryScope($fromTable, $joins, $where, $groupBy !== null);
-
-        // If we're selecting '*', get the selected columns from the table
-        if (\count($selectColumns) === 1 && $selectColumns[0]->getExpression() instanceof Asterisk) {
-            $selectColumns = [];
-            foreach ($fromTable->getColumns() as $column) {
-                $selectColumns[] = new SelectExpression(new SimpleName($column->getName()));
-            }
-        }
-
-        foreach ($selectColumns as $i => $column) {
-            $expression = $column->getExpression();
-
-            $offsetType = new ConstantIntegerType($i);
-
-            $nameType = null;
-            $exprName = self::getIdentifierName($column);
-            if ($exprName !== null) {
-                $nameType = new ConstantStringType($exprName);
-            }
-            $rawExpressionType = null;
-            if ($column->getRawExpression() !== null) {
-                $rawExpressionType = new ConstantStringType($column->getRawExpression());
-            }
-            $aliasOffsetType = null;
-            if (null !== $column->getAlias()) {
-                $aliasOffsetType = new ConstantStringType($column->getAlias());
-            }
-
-            $valueType = $resultType->getOffsetValueType($offsetType);
-
-            $type = $queryScope->getType($expression);
-            if (! $type instanceof MixedType) {
-                $valueType = $type;
-            }
-
-            if (null !== $rawExpressionType && $resultType->hasOffsetValueType($rawExpressionType)->yes()) {
-                $resultType = $resultType->setOffsetValueType(
-                    $rawExpressionType,
-                    $valueType
-                );
-            }
-            if (null !== $aliasOffsetType && $resultType->hasOffsetValueType($aliasOffsetType)->yes()) {
-                $resultType = $resultType->setOffsetValueType(
-                    $aliasOffsetType,
-                    $valueType
-                );
-            }
-            if (null !== $nameType && $resultType->hasOffsetValueType($nameType)->yes()) {
-                $resultType = $resultType->setOffsetValueType(
-                    $nameType,
-                    $valueType
-                );
-            }
-            if ($resultType->hasOffsetValueType($offsetType)->yes()) {
-                $resultType = $resultType->setOffsetValueType(
-                    $offsetType,
-                    $valueType
-                );
-            }
-        }
-
-        return $resultType;
+        throw new ShouldNotHappenException();
     }
 
     /**
