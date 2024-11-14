0e4ba8a001e30ec48e431b52c7584dc9b527f3e1
Markus Staab <maggus.staab@googlemail.com>
2024-11-14 16:13:08 +0100

    drop unnecessary list type

--- src/QueryReflection/PdoPgSqlQueryReflector.php
+++ src/QueryReflection/PdoPgSqlQueryReflector.php
@@ -13,7 +13,7 @@ use staabm\PHPStanDba\TypeMapping\TypeMapper;
 use function array_shift;
 
 /**
- * @phpstan-type PDOColumnMeta array{name: string, table?: string, native_type: string, len: int, flags: list<string>}
+ * @phpstan-type PDOColumnMeta array{name: string, table?: string, native_type: string, len: int, flags: array<int, string>}
  */
 final class PdoPgSqlQueryReflector extends BasePdoQueryReflector
 {
@@ -28,9 +28,9 @@ final class PdoPgSqlQueryReflector extends BasePdoQueryReflector
     }
 
     /**
-     * @return PDOException|list<PDOColumnMeta>|null
+     * @return PDOException|array<PDOColumnMeta>|null
      */
-    protected function simulateQuery(string $queryString) // @phpstan-ignore-line
+    protected function simulateQuery(string $queryString)
     {
         if (\array_key_exists($queryString, $this->cache)) {
             return $this->cache[$queryString];
