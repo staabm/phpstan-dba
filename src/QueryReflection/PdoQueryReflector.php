cf0685b70e1ad2970cb45ebf0e216cf264b4b3d2
Markus Staab <markus.staab@redaxo.de>
2025-03-06 16:58:40 +0100

    Update PdoQueryReflector.php

--- src/QueryReflection/PdoQueryReflector.php
+++ src/QueryReflection/PdoQueryReflector.php
@@ -10,8 +10,10 @@ use PDO;
  * This class was kept for BC reasons.
  *
  * @deprecated use PdoMysqlQueryReflector instead
+ * @phpstan-ignore class.extendsFinalByPhpDoc
  */
-final class PdoQueryReflector extends PdoMysqlQueryReflector { // @phpstan-ignore class.extendsFinalByPhpDoc
+final class PdoQueryReflector extends PdoMysqlQueryReflector
+{
     public function __construct(PDO $pdo)
     {
         parent::__construct($pdo);
