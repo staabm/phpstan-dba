ae4b1ba755b26a7fcc3687292b360508a8a632f4
Markus Staab <maggus.staab@googlemail.com>
2025-05-06 12:25:03 +0200

    Added missing `ReflectionProvider->hasClass()` checks (#758)

--- src/Rules/QueryPlanAnalyzerRule.php
+++ src/Rules/QueryPlanAnalyzerRule.php
@@ -78,7 +78,10 @@ final class QueryPlanAnalyzerRule implements Rule
             }
 
             if ($methodName === $methodReflection->getName() &&
-                ($methodReflection->getDeclaringClass()->getName() === $className || $methodReflection->getDeclaringClass()->isSubclassOfClass($this->reflectionProvider->getClass($className)))
+                (
+                    $methodReflection->getDeclaringClass()->getName() === $className
+                    || ($this->reflectionProvider->hasClass($className) && $methodReflection->getDeclaringClass()->isSubclassOfClass($this->reflectionProvider->getClass($className)))
+                )
             ) {
                 $unsupportedMethod = false;
                 break;
--- src/Rules/SyntaxErrorInQueryMethodRule.php
+++ src/Rules/SyntaxErrorInQueryMethodRule.php
@@ -62,7 +62,10 @@ final class SyntaxErrorInQueryMethodRule implements Rule
             }
 
             if ($methodName === $methodReflection->getName() &&
-                ($methodReflection->getDeclaringClass()->getName() === $className || $methodReflection->getDeclaringClass()->isSubclassOfClass($this->reflectionProvider->getClass($className)))
+                (
+                    $methodReflection->getDeclaringClass()->getName() === $className
+                    || ($this->reflectionProvider->hasClass($className) && $methodReflection->getDeclaringClass()->isSubclassOfClass($this->reflectionProvider->getClass($className)))
+                )
             ) {
                 $unsupportedMethod = false;
                 break;
