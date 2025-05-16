1de385063eb21bc692b63adcbfe8ce15601e0550
Bartłomiej Nowak <bartlomiej.nowak@workbuzz.com>
2025-05-16 13:35:50 +0200

    use not depracated Connection::executeQuery and Connection::executeStatement methods to validate queries syntax errors

--- tests/rules/data/syntax-error-in-query-method.php
+++ tests/rules/data/syntax-error-in-query-method.php
@@ -79,13 +79,13 @@ class Foo
     public function syntaxErrorDoctrineDbal(\Doctrine\DBAL\Connection $conn)
     {
         $sql = 'SELECT email adaid WHERE gesperrt freigabe1u1 FROM ada';
-        $conn->query($sql);
+        $conn->executeQuery($sql);
     }
 
     public function noErrorOnQueriesContainingPlaceholders(\Doctrine\DBAL\Connection $conn)
     {
         // errors in this scenario are reported by SyntaxErrorInPreparedStatementMethodRule only
-        $conn->query('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada WHERE adaid=?');
+        $conn->executeQuery('SELECT email, adaid, gesperrt, freigabe1u1 FROM ada WHERE adaid=?');
     }
 
     public function conditionalSyntaxError(PDO $pdo)
