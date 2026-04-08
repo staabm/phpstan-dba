82b43a7288ac72be1314f40851a6d086bc19cc43
Etienne V. Labelle <evaillancourt@vaielab.com>
2026-04-08 08:41:23 -0400

    test: add regression case for PDOStatement::execute returning mixed rows

--- tests/default/data/pdo-stmt-execute.php
+++ tests/default/data/pdo-stmt-execute.php
@@ -9,6 +9,7 @@ class Foo
 {
     public function execute(PDO $pdo)
     {
+        
         $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
         $stmt->execute([':adaid' => 1]);
         foreach ($stmt as $row) {
@@ -33,11 +34,12 @@ class Foo
             assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
         }
 
-        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ? and email = ?');
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ? and email = ? ');
         $stmt->execute([1, 'email@example.org']);
         foreach ($stmt as $row) {
             assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
         }
+        
     }
 
     public function executeWithBindCalls(PDO $pdo)
@@ -48,6 +50,43 @@ class Foo
         $stmt->bindParam(':test1', $test);
         $stmt->bindValue(':test2', 1001);
         $stmt->execute();
+
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
+        $stmt->bindValue(':adaid', 1);
+        $stmt->execute();
+        foreach ($stmt as $row) {
+            assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
+        }
+
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = :adaid');
+        $stmt->bindValue('adaid', 1);
+        $stmt->execute(); // prefixed ":" is optional
+        foreach ($stmt as $row) {
+            assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
+        }
+
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE email = :email');
+        $stmt->bindValue(':email', 'email@example.org');
+        $stmt->execute();
+        foreach ($stmt as $row) {
+            assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
+        }
+
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ?');
+        $stmt->bindValue(1, 1);
+        $stmt->execute();
+        foreach ($stmt as $row) {
+            assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
+        }
+
+        $stmt = $pdo->prepare('SELECT email, adaid FROM ada WHERE adaid = ? and email = ? ');
+        $stmt->bindValue(1, 1);
+        $stmt->bindValue(2, 'email@example.org');
+        $stmt->execute();
+        foreach ($stmt as $row) {
+            assertType('array{email: string, 0: string, adaid: int<-32768, 32767>, 1: int<-32768, 32767>}', $row);
+        }
+        
     }
 
     public function errors(PDO $pdo)
