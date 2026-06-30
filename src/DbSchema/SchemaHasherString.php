953f58c4551942069decfa212070dc9f8466713c
Markus Staab <markus.staab@redaxo.de>
2026-06-30 12:26:30 +0200

    fix

---
+++ src/DbSchema/SchemaHasherString.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace staabm\PHPStanDba\DbSchema;
+
+use mysqli;
+use PDO;
+use PHPStan\ShouldNotHappenException;
+
+final class SchemaHasherString implements SchemaHasher
+{
+    private string $hash;
+
+    public function __construct(string $hash)
+    {
+        $this->hash = $hash;
+    }
+
+    public function hashDb(): string
+    {
+        return $this->hash;
+    }
+}
