1df9354a7cd426e33917f29b2b48cf6f52df849f
Markus Staab <markus.staab@redaxo.de>
2026-06-30 14:26:43 +0200

    don re-write cache when nothing changed

--- src/QueryReflection/ReflectionCache.php
+++ src/QueryReflection/ReflectionCache.php
@@ -149,8 +149,9 @@ final class ReflectionCache
 
         if (! \is_array($cache) ||
             ! \array_key_exists('schemaVersion', $cache) ||
-            ! \array_key_exists('schemaHash', $cache) || // schemaHash exists but is null in recording mode
-            self::SCHEMA_VERSION !== $cache['schemaVersion']) {
+            ! \array_key_exists('schemaHash', $cache) || // schemaHash might be null in recording mode
+            self::SCHEMA_VERSION !== $cache['schemaVersion']
+        ) {
             return null;
         }
 
@@ -158,7 +159,6 @@ final class ReflectionCache
             return null;
         }
 
-        // the schemaHash is only available in replay-and-record mode.
         if (null === $this->schemaHash) {
             if ($cache['schemaHash'] !== null && ! is_string($cache['schemaHash'])) {
                 throw new ShouldNotHappenException();
@@ -200,8 +200,11 @@ final class ReflectionCache
             foreach ($newRecords as &$newRecord) {
                 ksort($newRecord);
             }
-
             unset($newRecord);
+
+            if ($newRecords === $cachedRecords) {
+                return;
+            }
 
             $cacheContent = '<?php return ' . var_export([
                 'schemaVersion' => self::SCHEMA_VERSION,
